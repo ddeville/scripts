17dc0440dac8a4d79277ded20eb3c2269379e382
Damien DeVille <damien@ddeville.me>
2014-09-15 19:10:43 +0100

    Nicer setup script
    Now the themes are linked rather than copied and they use the same
    function that takes care of replacing items.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-chflags nohidden ~/Library
-
 scripts_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function link_file
 {
-	file_name="${1%/}"
+	file_origin="${1%/}"
+	file_destination="${2%/}"
 	
-	if [ -z "${file_name}" ]
+	if [ -z "${file_origin}" ]
 	then
-		echo -e "> Need to specify a file name"
+		echo -e "> Need to specify a file origin"
 		return
 	fi
 	
-	file_location="${HOME}/${file_name}"
+	if [ -z "${file_destination}" ]
+	then
+		echo -e "> Need to specify a file destination"
+		return
+	fi
 	
-	if [ -e "${file_location}" ];
+	if [ -e "${file_destination}" ];
 	then
 		while [ 1 ]
 		do
-			read -r -p "> ${file_location} already exists, overwrite? [y/n]: "
+			read -r -p "> ${file_destination} already exists, overwrite? [y/n]: "
 			if [ "${REPLY}" != "y" -a "${REPLY}" != "n" ]
 			then
 				echo "> Unknown option ${REPLY}; try again" >&2
@@ -32,43 +35,46 @@ function link_file
 		
 		if [ "${REPLY}" = "y" ]
 		then
-			rm -rf "${file_location}"
-			echo "> Removed ${file_location}"
+			rm -rf "${file_destination}"
+			echo "> Removed ${file_destination}"
 		fi
 	fi
 	
-	file_origin="${scripts_directory}/dotfiles/${file_name}"
-	
-	ln -s "${file_origin}" "${file_location}"
-	echo "> Linked ${file_origin} to ${file_location}"
+	ln -s "${file_origin}" "${file_destination}"
+	echo "> Linked ${file_origin} to ${file_destination}"
 }
+
+# Library hidden
+
+chflags nohidden ~/Library
+
+echo "=== ~/Library is now visible ==="
 
 # Dot files
 
-link_file .profile
-link_file .lldbinit
-link_file .gitconfig
-link_file .vim
-link_file .vimrc
+dot_origin_folder="${scripts_directory}/dotfiles"
+dot_destination_folder="${HOME}"
 
-echo "=== Dot files copied ==="
+link_file "${dot_origin_folder}/.profile" "${dot_destination_folder}/.profile"
+link_file "${dot_origin_folder}/.lldbinit" "${dot_destination_folder}/.lldbinit"
+link_file "${dot_origin_folder}/.gitconfig" "${dot_destination_folder}/.gitconfig"
+link_file "${dot_origin_folder}/.vim" "${dot_destination_folder}/.vim"
+link_file "${dot_origin_folder}/.vimrc" "${dot_destination_folder}/.vimrc"
 
-# Xcode themes
+echo "=== Dot files linked ==="
+
+# Themes
+
+themes_origin_folder="${scripts_directory}/themes"
 
 xcode_themes_location="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes"
-
 mkdir "${xcode_themes_location}" > /dev/null 2>&1
-cp "${scripts_directory}/themes/Pastel.dvtcolortheme" "${xcode_themes_location}/Pastel.dvtcolortheme"
 
-echo "=== Xcode themes copied ==="
-
-# BBEdit
+link_file "${themes_origin_folder}/Pastel.dvtcolortheme" "${xcode_themes_location}/Pastel.dvtcolortheme"
 
 bbedit_themes_location="$HOME/Library/Application Support/BBEdit/Color Schemes"
-
 mkdir "${bbedit_themes_location}" > /dev/null 2>&1
-cp "${scripts_directory}/themes/Gruber.bbcolors" "${bbedit_themes_location}/Gruber.bbcolors"
 
-echo "=== BBEdit themes copied ==="
+link_file "${themes_origin_folder}/Gruber.bbcolors" "${bbedit_themes_location}/Gruber.bbcolors"
 
-
+echo "=== Themes linked ==="
