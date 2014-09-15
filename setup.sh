7ef25f500452a30a98d56e4e09bfcb07714f4995
Damien DeVille <damien@ddeville.me>
2014-09-15 13:26:09 +0100

    new setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ln -s ~/Scripts/.profile ~/.profile
+ln -s ~/Scripts/.lldbinit ~/.lldbinit
+ln -s ~/Scripts/.gitconfig ~/.gitconfig
+ln -s ~/Scripts/.vim ~/.vim
+ln -s ~/Scripts/.vimrc ~/.vimrc
+
