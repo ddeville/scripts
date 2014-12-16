06587cf66815604b653854c0a5894461e2b37f4c
Damien DeVille <damien@ddeville.me>
2014-12-16 15:51:11 +0000

    New Sublime Text prefs

--- setup.sh
+++ setup.sh
@@ -6,19 +6,19 @@ function link_file
 {
 	file_origin="${1%/}"
 	file_destination="${2%/}"
-	
+
 	if [ -z "${file_origin}" ]
 	then
 		echo -e "> Need to specify a file origin"
 		return
 	fi
-	
+
 	if [ -z "${file_destination}" ]
 	then
 		echo -e "> Need to specify a file destination"
 		return
 	fi
-	
+
 	if [ -e "${file_destination}" ];
 	then
 		while [ 1 ]
@@ -29,17 +29,17 @@ function link_file
 				echo "> Unknown option ${REPLY}; try again" >&2
 				continue
 			fi
-			
+
 			break
 		done
-		
+
 		if [ "${REPLY}" = "y" ]
 		then
 			rm -rf "${file_destination}"
 			echo "> Removed ${file_destination}"
 		fi
 	fi
-	
+
 	ln -s "${file_origin}" "${file_destination}"
 	echo "> Linked ${file_origin} to ${file_destination}"
 }
@@ -55,12 +55,14 @@ echo "=== ~/Library is now visible ==="
 dot_origin_folder="${scripts_directory}/dotfiles"
 dot_destination_folder="${HOME}"
 
+mkdir "${dot_destination_folder}/.vim/colors" > /dev/null 2>&1
+
 link_file "${dot_origin_folder}/.profile" "${dot_destination_folder}/.profile"
 link_file "${dot_origin_folder}/.bash_profile" "${dot_destination_folder}/.bash_profile"
 link_file "${dot_origin_folder}/.bashrc" "${dot_destination_folder}/.bashrc"
 link_file "${dot_origin_folder}/.lldbinit" "${dot_destination_folder}/.lldbinit"
 link_file "${dot_origin_folder}/.gitconfig" "${dot_destination_folder}/.gitconfig"
-link_file "${dot_origin_folder}/.vim" "${dot_destination_folder}/.vim"
+link_file "${dot_origin_folder}/.vim/colors/railscasts.vim" "${dot_destination_folder}/.vim/colors/railscasts.vim"
 link_file "${dot_origin_folder}/.vimrc" "${dot_destination_folder}/.vimrc"
 
 echo "=== Dot files linked ==="
@@ -78,6 +80,13 @@ bbedit_themes_location="$HOME/Library/Application Support/BBEdit/Color Schemes"
 mkdir "${bbedit_themes_location}" > /dev/null 2>&1
 
 link_file "${themes_origin_folder}/Gruber.bbcolors" "${bbedit_themes_location}/Gruber.bbcolors"
+
+sublime_text_settings_location="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+mkdir "${sublime_text_settings_location}" > /dev/null 2>&1
+
+link_file "${themes_origin_folder}/sublime-text/Preferences.sublime-settings" "${sublime_text_settings_location}/Preferences.sublime-settings"
+link_file "${themes_origin_folder}/sublime-text/Default (OSX).sublime-keymap" "${sublime_text_settings_location}/Default (OSX).sublime-keymap"
+link_file "${themes_origin_folder}/sublime-text/Tomorrow-Night-Eighties (SL).tmTheme" "${sublime_text_settings_location}/Tomorrow-Night-Eighties (SL).tmTheme"
 
 echo "=== Themes linked ==="
 
