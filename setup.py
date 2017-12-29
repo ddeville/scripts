8517084a243e2c6ca95b0a37aef6bd92e8ed7a84
Damien DeVille <damien@ddeville.me>
2017-12-28 16:29:11 -0800

    Add more stuff to the script

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+
+import os
+import subprocess
+import sys
+
+# first check (`xcode-select --version`) and install (`xcode-select --install`)
+# then check and install brew (`/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`)
+
+def is_cmd_installed(cmd):
+	try:
+		path = subprocess.check_output(["which", cmd]).strip()
+		return os.path.exists(path) and os.access(path, os.X_OK)
+	except subprocess.CalledProcessError as e:
+		return False
+
+# Let's first make sure that a few things are installed
+if not is_cmd_installed("brew"):
+	print("Install brew by running")
+
+print("DOG %r" % is_cmd_installed("brew"))
