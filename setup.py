84dcc7c4630430e6950330c07200658404591f70
Damien DeVille <damien@ddeville.me>
2018-01-04 16:36:06 -0800

    Add python3 to packages

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ PACKAGES_MACOS = [
     "fish",
     "the_silver_searcher",
     "tmux",
+    "python3",
 ]
 
 def setup_cmd_install_brew_formulas_if_needed():
@@ -78,6 +79,7 @@ PACKAGES_LINUX = [
     "silversearcher-ag",
     "tmux",
     "vim",
+    "python3",
 ]
 
 def setup_cmd_install_linux_packages_if_needed():
