69f9659a7cf75cb85bf7970f87b59fe452350b3f
Damien DeVille <damien@ddeville.me>
2018-04-16 13:26:18 -0700

    Prune more things

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -91,7 +91,11 @@ abbr -a adp "arc diff --preview"
 # setup `fzf`
 set -x FZF_TMUX 0
 set -x FZF_DEFAULT_OPTS "--height 40% --border --tabstop=4"
-set -x FZF_ALT_C_COMMAND "find . -type d -name '.git' -prune -o -type d -print | cut -d/ -f2-"
+set -x FZF_ALT_C_COMMAND "find .\
+                          -type d -name '.git' -prune -o\
+                          -type d -name '__pycache__' -prune -o\
+                          -type d -print\
+                          | cut -d/ -f2-"
 
 # setup `fzf` and `ag`
 if type -q fzf; and type -q ag
