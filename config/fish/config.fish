af14af8c295d93448ff155d9d49f65da54468676
Damien DeVille <damien@ddeville.me>
2019-08-02 09:05:08 -0700

    Fix fzf with rg

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -115,7 +115,7 @@ abbr -a ssh-fix "eval (ssh-agent -c); and ssh-add -K ~/.ssh/id_rsa"
 # setup `fzf`
 set -x FZF_TMUX 0
 set -x FZF_DEFAULT_OPTS "--height 40% --border --tabstop=4"
-set -x FZF_DEFAULT_COMMAND "rg --files --hidden --glob '!.git/*'"
+set -x FZF_DEFAULT_COMMAND "command rg --files --hidden --glob '!.git/*'"
 set -x FZF_CTRL_T_COMMAND "git ls-files"
 set -x FZF_ALT_C_COMMAND "find .\
                           -type d -name '.git' -prune -o\
