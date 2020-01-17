10f6015e7c530ad55f3c3dbf491bd851bb789b62
Damien DeVille <damien@ddeville.me>
2020-01-16 16:01:03 -0800

    Spacing

--- config/fish/functions/dbx_rustup.fish
+++ config/fish/functions/dbx_rustup.fish
@@ -1,7 +1,7 @@
 function dbx_rustup
     set deps_path "deps/dependency_versions.bzl"
     if test -e $deps_path
-        set nightly_version (cat $deps_path | grep RUSTC_VERSION|grep -oE '[0-9]{4}(-[0-9]{2}){2}')
+        set nightly_version (cat $deps_path | grep RUSTC_VERSION | grep -oE '[0-9]{4}(-[0-9]{2}){2}')
         rustup toolchain add nightly-$nightly_version
         rustup override set nightly-$nightly_version
     else
