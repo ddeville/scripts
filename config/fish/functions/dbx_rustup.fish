30ca293e03d3f404d185d33f587a9ab3eac856d3
Damien DeVille <damien@ddeville.me>
2020-03-03 20:05:10 -0800

    Fix rustup function

--- config/fish/functions/dbx_rustup.fish
+++ config/fish/functions/dbx_rustup.fish
@@ -1,9 +1,9 @@
 function dbx_rustup --description "Install the current rCLIENT Rust version"
-    set deps_path "deps/dependency_versions.bzl"
-    if test -e $deps_path
-        set nightly_version (cat $deps_path | grep RUSTC_VERSION | grep -oE '[0-9]{4}(-[0-9]{2}){2}')
-        rustup toolchain add nightly-$nightly_version
-        rustup override set nightly-$nightly_version
+    set toolchain_path "rust-toolchain"
+    if test -e $toolchain_path
+        set toolchain (cat $toolchain_path)
+        rustup toolchain add $toolchain
+        rustup override set $toolchain
     else
         echo "You need to run this command in your client repo!"
     end
