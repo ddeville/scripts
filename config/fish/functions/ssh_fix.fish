0f39ab0d4362b2b5e03e2d1661371d3b57e0268e
Damien DeVille <damien@ddeville.me>
2020-03-22 01:17:09 +0000

    Fix ssh_fix on linux and ignore old fishd stuff

--- config/fish/functions/ssh_fix.fish
+++ config/fish/functions/ssh_fix.fish
@@ -1,4 +1,9 @@
 function ssh_fix --description "Load and add the current SSH key to the session"
     eval (ssh-agent -c)
-    ssh-add -K ~/.ssh/id_rsa
+    switch (uname)
+        case Darwin
+            ssh-add -K ~/.ssh/id_rsa
+        case '*'
+            ssh-add ~/.ssh/id_rsa
+    end
 end
