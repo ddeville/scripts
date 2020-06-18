69c2cc250dbc5dccad26e7727cb1ae0f91634209
Damien DeVille <damien@ddeville.me>
2020-06-18 09:23:46 -0700

    Fix double space

--- config/fish/functions/dbx_vpn.fish
+++ config/fish/functions/dbx_vpn.fish
@@ -2,12 +2,12 @@ function dbx_vpn
     set port 45623
     set server damien@damien-mbp.local
 
-    echo "Disabling proxy on port " $port
+    echo "Disabling proxy on port" $port
     command networksetup -setsocksfirewallproxy Wi-Fi localhost $port
     command networksetup -setsocksfirewallproxystate Wi-Fi on
 
-    echo "SSH connecting to " $server
-    echo "SSH binding to port " $port
+    echo "SSH connecting to" $server
+    echo "SSH binding to port" $port
     command ssh -D $port -N $server
 
     echo "Disabling SOCKS proxy"
