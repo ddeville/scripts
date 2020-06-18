3c2ad0cc47b279d5874f8e9af27758cbb39c0a69
Damien DeVille <damien@ddeville.me>
2020-06-18 09:20:37 -0700

    Enable proxy as part of vpn connection

--- config/fish/functions/dbx_vpn.fish
+++ config/fish/functions/dbx_vpn.fish
@@ -1,10 +1,15 @@
 function dbx_vpn
-    if count $argv > /dev/null
-        while true
-            ssh -D $argv[1] -N damien@damien-mbp.local
-            sleep 5
-        end
-    else
-        echo "You need to pass a port to bind!"
-    end
+    set port 45623
+    set server damien@damien-mbp.local
+
+    echo "Disabling proxy on port " $port
+    command networksetup -setsocksfirewallproxy Wi-Fi localhost $port
+    command networksetup -setsocksfirewallproxystate Wi-Fi on
+
+    echo "SSH connecting to " $server
+    echo "SSH binding to port " $port
+    command ssh -D $port -N $server
+
+    echo "Disabling SOCKS proxy"
+    command networksetup -setsocksfirewallproxystate Wi-Fi off
 end
