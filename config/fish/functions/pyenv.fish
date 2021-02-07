6e8f06f92f1d076890ce10958a303c343d91cd6b
Damien DeVille <damien@ddeville.me>
2021-02-06 18:04:19 -0800

    Correctly link openssl when building python

--- config/fish/functions/pyenv.fish
+++ config/fish/functions/pyenv.fish
@@ -5,6 +5,24 @@ function pyenv
     switch "$command"
     case rehash shell
         source (pyenv "sh-$command" $argv|psub)
+    case install
+        # Homebrew doesn't add symlinks for openssl on macOS since they already
+        # install LibreSSL and they'd conflict.
+        # Since building Python requires OpenSSL let's add these paths.
+        if test (uname) = "Darwin"
+            if test -e "/opt/homebrew/opt/openssl"
+                set -gx LDFLAGS "-L/opt/homebrew/opt/openssl/lib"
+                set -gx CPPFLAGS "-I/opt/homebrew/opt/openssl/include"
+                echo "Note: Adding Homebrew OpenSSL in /opt/homebrew/opt/openssl to compile flags (see ~/.config/fish/functions/pyenv.fish for more info)"
+            else if test -e "/usr/local/opt/openssl"
+                set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
+                set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
+                echo "Note: Adding Homebrew OpenSSL in /use/local/opt/openssl to compile flags (see ~/.config/fish/functions/pyenv.fish for more info)"
+            else
+                echo "Note: OpenSSL is not installed with Homebrew, building Python will likely fail"
+            end
+        end
+        command pyenv "$command" $argv
     case '*'
         command pyenv "$command" $argv
     end
