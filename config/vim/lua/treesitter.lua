58ccd4ac8ddb31c4f5445c078a9f92e9abebdbf8
Damien DeVille <damien@ddeville.me>
2021-05-30 15:37:23 -0700

    Move lua config to lua files

---
+++ config/vim/lua/treesitter.lua
@@ -0,0 +1,6 @@
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "cpp", "fish", "go", "java", "javascript", "lua", "python", "rust", "toml", "typescript"},
+    highlight = {
+        enable = true,
+    },
+}
