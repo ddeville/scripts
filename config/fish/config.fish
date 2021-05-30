73ac30b45c8feefb6c3d69aeecf7be7a23dbde02
Damien DeVille <damien@ddeville.me>
2021-05-30 15:04:48 -0700

    Only search current dir

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -133,8 +133,8 @@ alias adu="arc diff --update"
 # setup `fzf`
 set -x FZF_TMUX 0
 set -x FZF_DEFAULT_OPTS "--height 40% --border --tabstop=4"
-set -x FZF_DEFAULT_COMMAND "fd --exclude .git --hidden . $HOME"
+set -x FZF_DEFAULT_COMMAND "fd --exclude .git --hidden --color=never"
 set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-set -x FZF_ALT_C_COMMAND "fd --type d --exclude .git --exclude __pycache__ --hidden . $HOME"
+set -x FZF_ALT_C_COMMAND "fd --type d --exclude .git --hidden --color=never"
 # alt-c doesn't work on macos so rather than tweaking the iterm settings bind it
 bind -M insert "ç" fzf-cd-widget
