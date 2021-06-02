9edaea441d7d94898f7b7a5c4f9eb71f967c724e
Damien DeVille <damien@ddeville.me>
2021-06-01 20:24:42 -0700

    Move require to a local

--- config/vim/lua/treesitter_config.lua
+++ config/vim/lua/treesitter_config.lua
@@ -1,4 +1,6 @@
-require'nvim-treesitter.configs'.setup {
+local treesitter = require('nvim-treesitter.configs')
+
+treesitter.setup {
   ensure_installed = { "c", "cpp", "fish", "go", "java", "javascript", "lua", "python", "rust", "toml", "typescript"},
   highlight = {
     enable = true,
