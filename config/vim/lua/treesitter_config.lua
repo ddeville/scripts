8140b2b802d970385e171bcc64e0a9ce3ab5a501
Damien DeVille <damien@ddeville.me>
2021-06-01 21:25:38 -0700

    Add a few more languages to treesitter

--- config/vim/lua/treesitter_config.lua
+++ config/vim/lua/treesitter_config.lua
@@ -2,17 +2,24 @@ local treesitter = require("nvim-treesitter.configs")
 
 treesitter.setup {
   ensure_installed = {
+    "bash",
     "c",
+    "comment",
     "cpp",
+    "css",
+    "dockerfile",
     "fish",
     "go",
+    "html",
     "java",
     "javascript",
+    "json",
     "lua",
     "python",
     "rust",
     "toml",
     "typescript",
+    "yaml",
   },
   highlight = {
     enable = true,
