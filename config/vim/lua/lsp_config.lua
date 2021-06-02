2560fcc324b5e29fa53bd573487c06b09de38f03
Damien DeVille <damien@ddeville.me>
2021-06-02 16:21:22 -0700

    Add some more keymaps

--- config/vim/lua/lsp_config.lua
+++ config/vim/lua/lsp_config.lua
@@ -3,14 +3,14 @@ local nvim_lsp = require("lspconfig")
 local on_generic_client_attach = function(client, bufnr)
   -- Mappings.
   local key_opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-j>", "<Cmd>lua vim.lsp.buf.definition()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd",       "<cmd>lua vim.lsp.buf.definition()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr",       "<cmd>lua vim.lsp.buf.references()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K",        "<cmd>lua vim.lsp.buf.hover()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>",    "<cmd>lua vim.lsp.buf.signature_help()<CR>", key_opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", key_opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d",       "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d",       "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", key_opts)
 
   -- Diagnostic settings
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
@@ -26,32 +26,32 @@ local on_generic_client_attach = function(client, bufnr)
   )
 
   -- Configure sign column for diagnostic messages
-  vim.fn.sign_define("LspDiagnosticsSignError", { text = "E", texthl = "LspDiagnosticsDefaultError" })
-  vim.fn.sign_define("LspDiagnosticsSignWarning", { text = "W", texthl = "LspDiagnosticsDefaultWarning" })
-  vim.fn.sign_define("LspDiagnosticsSignInformation", { text = "I", texthl = "LspDiagnosticsDefaultInformation" })
-  vim.fn.sign_define("LspDiagnosticsSignHint", { text = "H", texthl = "LspDiagnosticsDefaultHint" })
+  vim.fn.sign_define("LspDiagnosticsSignError",         { text = "E", texthl = "LspDiagnosticsDefaultError" })
+  vim.fn.sign_define("LspDiagnosticsSignWarning",       { text = "W", texthl = "LspDiagnosticsDefaultWarning" })
+  vim.fn.sign_define("LspDiagnosticsSignInformation",   { text = "I", texthl = "LspDiagnosticsDefaultInformation" })
+  vim.fn.sign_define("LspDiagnosticsSignHint",          { text = "H", texthl = "LspDiagnosticsDefaultHint" })
 
   -- Colorscheme for diagnostic messages
   -- Error in Red
-  vim.api.nvim_command("highlight LspDiagnosticsDefaultError guifg=Red ctermfg=Red")
-  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextError guifg=Red ctermfg=Red")
-  vim.api.nvim_command("highlight LspDiagnosticsSignError guifg=Red ctermfg=Red")
-  vim.api.nvim_command("highlight LspDiagnosticsUnderlineError guifg=Red ctermfg=Red")
+  vim.api.nvim_command("highlight LspDiagnosticsDefaultError            guifg=Red ctermfg=Red")
+  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextError        guifg=Red ctermfg=Red")
+  vim.api.nvim_command("highlight LspDiagnosticsSignError               guifg=Red ctermfg=Red")
+  vim.api.nvim_command("highlight LspDiagnosticsUnderlineError          guifg=Red ctermfg=Red")
   -- Warning in Yellow
-  vim.api.nvim_command("highlight LspDiagnosticsDefaultWarning guifg=Yellow ctermfg=Yellow")
-  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextWarning guifg=Yellow ctermfg=Yellow")
-  vim.api.nvim_command("highlight LspDiagnosticsSignWarning guifg=Yellow ctermfg=Yellow")
-  vim.api.nvim_command("highlight LspDiagnosticsUnderlineWarning guifg=Red ctermfg=Red")
+  vim.api.nvim_command("highlight LspDiagnosticsDefaultWarning          guifg=Yellow ctermfg=Yellow")
+  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextWarning      guifg=Yellow ctermfg=Yellow")
+  vim.api.nvim_command("highlight LspDiagnosticsSignWarning             guifg=Yellow ctermfg=Yellow")
+  vim.api.nvim_command("highlight LspDiagnosticsUnderlineWarning        guifg=Red ctermfg=Red")
   -- Info in White
-  vim.api.nvim_command("highlight LspDiagnosticsDefaultInformation guifg=White ctermfg=White")
-  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextInformation guifg=White ctermfg=White")
-  vim.api.nvim_command("highlight LspDiagnosticsSignInformation guifg=White ctermfg=White")
-  vim.api.nvim_command("highlight LspDiagnosticsUnderlineInformation guifg=White ctermfg=White")
+  vim.api.nvim_command("highlight LspDiagnosticsDefaultInformation      guifg=White ctermfg=White")
+  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextInformation  guifg=White ctermfg=White")
+  vim.api.nvim_command("highlight LspDiagnosticsSignInformation         guifg=White ctermfg=White")
+  vim.api.nvim_command("highlight LspDiagnosticsUnderlineInformation    guifg=White ctermfg=White")
   -- Hint in Gray
-  vim.api.nvim_command("highlight LspDiagnosticsDefaultHint guifg=Gray ctermfg=Gray")
-  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextHint guifg=Gray ctermfg=Gray")
-  vim.api.nvim_command("highlight LspDiagnosticsSignHint guifg=Gray ctermfg=Gray")
-  vim.api.nvim_command("highlight LspDiagnosticsUnderlineHint guifg=Gray ctermfg=Gray")
+  vim.api.nvim_command("highlight LspDiagnosticsDefaultHint             guifg=Gray ctermfg=Gray")
+  vim.api.nvim_command("highlight LspDiagnosticsVirtualTextHint         guifg=Gray ctermfg=Gray")
+  vim.api.nvim_command("highlight LspDiagnosticsSignHint                guifg=Gray ctermfg=Gray")
+  vim.api.nvim_command("highlight LspDiagnosticsUnderlineHint           guifg=Gray ctermfg=Gray")
 end
 
 nvim_lsp.rust_analyzer.setup({
@@ -120,16 +120,16 @@ nvim_lsp.sourcekit.setup({
 
 nvim_lsp.sumneko_lua.setup({
   on_attach = on_generic_client_attach;
-  cmd = {"/opt/lsp/lua-language-server"};
+  cmd = { "/opt/lsp/lua-language-server" };
   settings = {
     Lua = {
       runtime = {
         version = "LuaJIT";
-        path = vim.list_extend(vim.split(package.path, ";"), {"lua/?.lua", "lua/?/init.lua"});
+        path = vim.list_extend(vim.split(package.path, ";"), { "lua/?.lua", "lua/?/init.lua" });
       };
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {"vim"};
+        globals = { "vim" };
       };
       workspace = {
         -- Make the server aware of Neovim runtime files
