569f221cddead3cafa5b30344e1c1205703a7a65
Damien DeVille <damien@ddeville.me>
2021-06-02 18:24:58 -0700

    A bunch of new mappings

--- config/vim/lua/config/lsp.lua
+++ config/vim/lua/config/lsp.lua
@@ -3,14 +3,18 @@ local nvim_lsp = require("lspconfig")
 local on_generic_client_attach = function(client, bufnr)
   -- Mappings.
   local key_opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd",       "<cmd>lua vim.lsp.buf.definition()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr",       "<cmd>lua vim.lsp.buf.references()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K",        "<cmd>lua vim.lsp.buf.hover()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>",    "<cmd>lua vim.lsp.buf.signature_help()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d",       "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", key_opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d",       "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd",        "<cmd>lua vim.lsp.buf.definition()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr",        "<cmd>lua vim.lsp.buf.references()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi",        "<cmd>lua vim.lsp.buf.implementation()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gy",        "<cmd>lua vim.lsp.buf.type_definition()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K",         "<cmd>lua vim.lsp.buf.hover()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>",     "<cmd>lua vim.lsp.buf.signature_help()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>a",  "<cmd>lua vim.lsp.buf.code_action()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>e",  "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>q",  "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d",        "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", key_opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d",        "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", key_opts)
 
   -- Diagnostic settings
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
