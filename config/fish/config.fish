e0015b8ac8813ea8433e14d3f959a36037c9d00e
Damien DeVille <damien@ddeville.me>
2021-06-11 20:39:55 -0700

    Set XDG env variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,9 +3,14 @@ set -gx CLICOLOR 1
 set -gx GREP_OPTIONS "--color=auto"
 set -gx VIMINIT "source ~/.vim/vimrc"
 
+# XDG Base Directory
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+
 # setup the base16 shell and source the colors
 if status --is-interactive
-    source "$HOME/.config/base16-shell/profile_helper.fish"
+    source "$XDG_CONFIG_HOME/base16-shell/profile_helper.fish"
 end
 
 # setup the fish colorscheme (Tomorrow Night Bright from the fish web config)
