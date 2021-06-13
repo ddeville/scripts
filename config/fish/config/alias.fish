ed0a3c86de56c53ae47aeec6bb5959330342ff10
Damien DeVille <damien@ddeville.me>
2021-06-13 11:53:21 -0700

    Make oo an alias rather than an abbr

--- config/fish/config/alias.fish
+++ config/fish/config/alias.fish
@@ -7,10 +7,10 @@ alias gaa="git add .; and git ci"
 alias gaaa="git add .; and git ci --amend"
 alias adp="arc diff --preview"
 alias adu="arc diff --update"
+alias oo="open ."
 
 # abbreviations
 abbr -a cdd "cd .."
-abbr -a oo "open ."
 abbr -a con "tail -40 -f /var/log/system.log"
 abbr -a topc "top -o cpu"
 abbr -a topm "top -o mem"
