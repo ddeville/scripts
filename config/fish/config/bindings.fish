823d541ce60519f7cb980e0b1dfa870f641b3a77
Damien DeVille <damien@ddeville.me>
2021-06-13 11:42:00 -0700

    Move fish bindings to new file

---
+++ config/fish/config/bindings.fish
@@ -0,0 +1,14 @@
+# use the vim key bindings
+set -gx fish_key_bindings fish_vi_key_bindings
+
+# ctrl-y to accept autocomplete suggestion
+bind -M insert \cy accept-autosuggestion
+
+# setup `fzf`
+set -x FZF_TMUX 0
+set -x FZF_DEFAULT_OPTS "--height 40% --border --tabstop=4"
+set -x FZF_DEFAULT_COMMAND "fd --exclude .git --hidden --color=never"
+set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -x FZF_ALT_C_COMMAND "fd --type d --exclude .git --hidden --color=never"
+# alt-c doesn't work on macos so rather than tweaking the iterm settings bind it
+bind -M insert "ç" fzf-cd-widget
