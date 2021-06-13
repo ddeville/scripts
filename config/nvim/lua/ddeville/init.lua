fde509fc27b5e198e0281382f529b384a5d3d9c7
Damien DeVille <damien@ddeville.me>
2021-06-12 18:10:57 -0700

    Style

--- config/nvim/lua/ddeville/init.lua
+++ config/nvim/lua/ddeville/init.lua
@@ -3,7 +3,7 @@ local M = {}
 -- Add support for reporting progress in the ligthline (see `LspStatus` in vimrc)
 M.lsp_status_message = function()
   if #vim.lsp.buf_get_clients(0) > 0 then
-    local msg = require'lsp-status'.status_progress()
+    local msg = require('lsp-status').status_progress()
     -- Try to prevent the status message from overflowing and thus moving all status items to the left.
     local space = vim.fn.winwidth(0) - 70
     if #msg > space then
