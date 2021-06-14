5037494e04f6cddd99953999a44c361011a6e418
Damien DeVille <damien@ddeville.me>
2021-06-13 19:38:38 -0700

    DRY

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,11 +3,11 @@ set -gx CLICOLOR 1
 set -gx GREP_OPTIONS "--color=auto"
 set -gx VIMINIT "source ~/.vim/vimrc"
 
-# XDG Base Directory
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 
-if status --is-interactive && test -e "$XDG_CONFIG_HOME/base16-shell/profile_helper.fish"
-    source "$XDG_CONFIG_HOME/base16-shell/profile_helper.fish"
+set -l base16_path "$XDG_CONFIG_HOME/base16-shell/profile_helper.fish"
+if status --is-interactive && test -e $base16_path
+    source $base16_path
 end
