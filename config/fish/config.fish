a71f339d316f6576c2ceae399d15ecb4db2f359a
Damien DeVille <damien@ddeville.me>
2021-06-14 10:19:47 -0700

    Only set XDG paths if they haven't been set

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,9 +3,15 @@ set -gx CLICOLOR 1
 set -gx GREP_OPTIONS "--color=auto"
 set -gx VIMINIT "source ~/.vim/vimrc"
 
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx XDG_DATA_HOME "$HOME/.local/share"
+if not set -q XDG_CONFIG_HOME
+    set -gx XDG_CONFIG_HOME "$HOME/.config"
+end
+if not set -q XDG_CACHE_HOME
+    set -gx XDG_CACHE_HOME "$HOME/.cache"
+end
+if not set -q XDG_DATA_HOME
+    set -gx XDG_DATA_HOME "$HOME/.local/share"
+end
 
 set -l base16_path "$XDG_CONFIG_HOME/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
