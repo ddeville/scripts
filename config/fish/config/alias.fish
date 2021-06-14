b7dddfcbbe813304320f8a4fbe04d1eb198edc8b
Damien DeVille <damien@ddeville.me>
2021-06-13 18:47:24 -0700

    Apparently this is faster

--- config/fish/config/alias.fish
+++ config/fish/config/alias.fish
@@ -1,13 +1,13 @@
 # aliases
-alias e="nvim"
-alias realvim=(which vim)
-alias vim="nvim"
-alias ll="ls -lah"
-alias gaa="git add .; and git ci"
-alias gaaa="git add .; and git ci --amend"
-alias adp="arc diff --preview"
-alias adu="arc diff --update"
-alias oo="open ."
+alias e "nvim"
+alias realvim (which vim)
+alias vim "nvim"
+alias ll "ls -lah"
+alias gaa "git add .; and git ci"
+alias gaaa "git add .; and git ci --amend"
+alias adp "arc diff --preview"
+alias adu "arc diff --update"
+alias oo "open ."
 
 # abbreviations
 abbr -a cdd "cd .."
