7b3c87de5a8a8b6782be2ed78134021cf159103f
Damien DeVille <damien@ddeville.me>
2021-06-13 19:12:08 -0700

    Only source if file exists

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,6 +8,6 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 
-if status --is-interactive
+if status --is-interactive && test -e "$XDG_CONFIG_HOME/base16-shell/profile_helper.fish"
     source "$XDG_CONFIG_HOME/base16-shell/profile_helper.fish"
 end
