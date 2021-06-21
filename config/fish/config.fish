3e762fb479f2404999294b37eec44e4b366fbb1c
Damien DeVille <damien@ddeville.me>
2021-06-20 17:03:46 -0700

    Add XDG to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,20 +1,28 @@
-set -gx EDITOR nvim
-set -gx CLICOLOR 1
-set -gx GREP_OPTIONS "--color=auto"
-
+# XDG Base Directory Specification
 if not set -q XDG_CONFIG_HOME
     set -gx XDG_CONFIG_HOME "$HOME/.config"
 end
+command mkdir -p $XDG_CONFIG_HOME
+
 if not set -q XDG_CACHE_HOME
     set -gx XDG_CACHE_HOME "$HOME/.cache"
 end
+command mkdir -p $XDG_CACHE_HOME
+
 if not set -q XDG_DATA_HOME
     set -gx XDG_DATA_HOME "$HOME/.local/share"
 end
+
+command mkdir -p $XDG_DATA_HOME
+
 if not set -q XDG_STATE_HOME
     set -gx XDG_STATE_HOME "$HOME/.local/state"
 end
+command mkdir -p $XDG_STATE_HOME
 
+set -gx EDITOR nvim
+set -gx CLICOLOR 1
+set -gx GREP_OPTIONS "--color=auto"
 set -gx VIMINIT "source ~/.vim/vimrc"
 set -gx MYVIMRC "$XDG_CONFIG_HOME/nvim/init.vim"
 set -gx PYENV_SHELL fish
