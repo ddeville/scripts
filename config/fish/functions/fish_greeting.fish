d264b1c8ed09e5ac9c4e2efd261573e02b6b42bb
Damien DeVille <damien@ddeville.me>
2021-06-22 19:32:02 -0700

    Print user and align

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -21,26 +21,32 @@ function fish_greeting
     set -l r_color (set_color normal; set_color green)
 
     if [ (uname -s) = "Darwin" ]
-        echo -n $l_color" OS: "
+        echo -n $l_color" OS:         "
         echo $r_color(sw_vers -productName) $r_color(sw_vers -productVersion) $r_color(sw_vers -buildVersion)
 
-        echo -n $l_color" Uptime: "
+        echo -n $l_color" Uptime:     "
         echo $r_color(uptime | sed -E 's/.*(up.*), [[:digit:]]+ user.*/\1/')
 
         echo -n $l_color" Disk usage: "
         echo $r_color(df -H | grep '/System/Volumes/Data$' | awk '{printf "%s available (%s)\n", $4, $5}')
 
-        echo -n $l_color" Hostname: "
+        echo -n $l_color" User:       "
+        echo $r_color(id -un)
+
+        echo -n $l_color" Hostname:   "
         echo $r_color(uname -n)
     else
-        echo -n $l_color" OS: "
+        echo -n $l_color" OS:       "
         echo $r_color(uname -rs)
 
-        echo -n $l_color" Uptime: "
+        echo -n $l_color" Uptime:   "
         echo $r_color(uptime -p)
 
         echo -n $l_color" Hostname: "
         echo $r_color(uname -n)
+
+        echo -n $l_color" User:     "
+        echo $r_color(id -un)
     end
 
     echo
