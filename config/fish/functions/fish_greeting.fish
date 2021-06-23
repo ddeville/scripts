dd5428c530ba7ad6ca59ed137c524984978f83ca
Damien DeVille <damien@ddeville.me>
2021-06-22 18:29:13 -0700

    Better greeting on mac

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -20,14 +20,25 @@ function fish_greeting
     set -l l_color (set_color normal; set_color --bold white)
     set -l r_color (set_color normal; set_color green)
 
-    echo -n $l_color" OS: "
-    echo $r_color(uname -rs)
+    if [ (uname -s) = "Darwin" ]
+        echo -n $l_color" OS: "
+        echo $r_color(sw_vers -productName) $r_color(sw_vers -productVersion) $r_color(sw_vers -buildVersion)
 
-    echo -n $l_color" Hostname: "
-    echo $r_color(uname -n)
+        echo -n $l_color" Uptime: "
+        echo $r_color(uptime | sed -E 's/.*(up.*), [[:digit:]]+ user.*/\1/')
 
-    echo -n $l_color" Uptime: "
-    echo $r_color(uptime)
+        echo -n $l_color" Hostname: "
+        echo $r_color(uname -n)
+    else
+        echo -n $l_color" OS: "
+        echo $r_color(uname -rs)
+
+        echo -n $l_color" Uptime: "
+        echo $r_color(uptime -p)
+
+        echo -n $l_color" Hostname: "
+        echo $r_color(uname -n)
+    end
 
     echo
 end
