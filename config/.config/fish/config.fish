c2a450d4f54b2a44c54f9d865d9c3c6f45ca0131
Damien DeVille <damien@ddeville.me>
2021-06-23 21:24:56 -0700

    More comments and move a couple of things around

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,8 +1,16 @@
-set -gx EDITOR nvim
 set -gx CLICOLOR 1
+set -gx TERM xterm-256color
+set -gx EDITOR nvim
 set -gx PYENV_SHELL fish
 
 set -l base16_path "$XDG_DATA_HOME/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
     source $base16_path
+end
+
+# GOPATH for Dropbox server code
+if test -e "$HOME/src/server/go"
+    if not set -q GOPATH || not contains "$HOME/src/server/go" "$GOPATH"
+        set -x GOPATH $GOPATH "$HOME/src/server/go"
+    end
 end
