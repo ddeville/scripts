8323450b3a4f3f754389006c3bfcdbfd4bf62cb0
Damien DeVille <damien@ddeville.me>
2021-06-23 17:33:56 -0700

    Handle gracefully if fzf is not yet installed

--- config/.config/fish/functions/fish_user_key_bindings.fish
+++ config/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
-    source $XDG_DATA_HOME/fzf/shell/key-bindings.fish
-    fzf_key_bindings
+    if test -e $XDG_DATA_HOME/fzf/shell/key-bindings.fish
+        source $XDG_DATA_HOME/fzf/shell/key-bindings.fish
+        fzf_key_bindings
+    end
 end
