6d171e78dc4b203b23e33b0b1ea5e1e5919791cb
Damien DeVille <damien@ddeville.me>
2021-08-05 23:52:03 -0700

    Support new ssh key algo

--- config/.config/fish/functions/ssh_fix.fish
+++ config/.config/fish/functions/ssh_fix.fish
@@ -1,9 +1,16 @@
 function ssh_fix --description "Load and add the current SSH key to the session"
     eval (ssh-agent -c)
+    if test -e ~/.ssh/id_rsa
+        set path ~/.ssh/id_rsa
+    else if test -e ~/.ssh/id_ed25519
+        set path ~/.ssh/id_ed25519
+    else
+        set path "MISSING_SSH_KEY"
+    end
     switch (uname)
         case Darwin
-            ssh-add -K ~/.ssh/id_rsa
+            ssh-add -K $path
         case '*'
-            ssh-add ~/.ssh/id_rsa
+            ssh-add $path
     end
 end
