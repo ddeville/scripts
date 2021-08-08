3d23f50542c6cd0ca4825b100feff07839098854
Damien DeVille <damien@ddeville.me>
2021-08-07 21:37:27 -0700

    Write environment variables in correct place

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -2,7 +2,6 @@ set -gx CLICOLOR 1
 set -gx TERM xterm-256color
 set -gx EDITOR nvim
 set -gx PYENV_SHELL fish
-set -gx SXHKD_SHELL sh
 
 set -l base16_path "$XDG_DATA_HOME/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
