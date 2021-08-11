944bd6f8ecd27128e66a808422c24405ae5e572d
Damien DeVille <damien@ddeville.me>
2021-08-11 13:41:38 -0700

    Move prefs to install script

--- install/macos/install.sh
+++ install/macos/install.sh
@@ -26,3 +26,33 @@ curl -fsSL https://sh.rustup.rs | /bin/sh -s -- -y --no-modify-path
 
 git clone https://github.com/ddeville/base16-shell.git ~/.local/share/base16-shell
 git clone https://github.com/tmux-plugins/tpm ~/scripts/config/common/.config/tmux/plugins/tpm
+
+chflags nohidden ~/Library
+chflags hidden ~/Applications
+
+defaults write com.apple.loginwindow TALLogoutSavesState -bool true
+
+defaults write NSGlobalDomain NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool true
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+defaults write com.apple.dock wvous-bl-corner -int 4
+defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 3
+defaults write com.apple.dock wvous-br-modifier -int 0
+defaults write com.apple.dock wvous-tl-corner -int 11
+defaults write com.apple.dock wvous-tl-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 2
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/scripts/macos/iterm"
+
+killall Dock
