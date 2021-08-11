7543c9ddca7054257430ec156ab6cea5c7d3cd2a
Damien DeVille <damien@ddeville.me>
2021-08-10 22:45:27 -0700

    More packages

--- linux/archinstall/x.sh
+++ linux/archinstall/x.sh
@@ -7,7 +7,7 @@ sudo pacman -S --needed \
     bspwm sxhkd slock lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
     flatpak dunst nautilus redshift firefox-developer-edition neofetch playerctl dmenu rofi sxiv mupdf \
     libvncserver remmina \
-    alacritty neovim stow bat exa fish htop jq ripgrep tmux curl fd go ninja nodejs npm \
+    alacritty neovim stow bat exa fish htop jq ripgrep tmux curl fd go ninja nodejs npm docker \
     faenza-icon-theme papirus-icon-theme \
     ttf-anonymous-pro ttf-liberation inter-font ttf-dejavu noto-fonts noto-fonts-emoji tex-gyre-fonts gnu-free-fonts xorg-fonts-misc
 
@@ -22,6 +22,6 @@ popd
 popd
 
 yay -S --needed \
-    polybar google-chrome 1password \
+    polybar google-chrome 1password spotify slack discord \
     dropbox nautilus-dropbox dropbox-cli \
     ttf-nunito montserrat-font-ttf otf-public-sans ttf-mac-fonts ttf-font-awesome
