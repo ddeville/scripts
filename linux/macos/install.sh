3305f0d2e433b9f5dd6f4521ce4bc5759205358c
Damien DeVille <damien@ddeville.me>
2021-08-11 12:08:39 -0700

    More work on macos install script

--- linux/macos/install.sh
+++ linux/macos/install.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-# /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 arch_name="$(uname -m)"
 if [ "${arch_name}" = "arm64" ]; then
-    brew_path=/opt/homebrew/bin/brew
+    brew_path=/opt/homebrew/bin
 else
-    brew_path=/usr/local/bin/brew
+    brew_path=/usr/local/bin
 fi
 
-$brew_path install \
-    bat exa fish htop jq neovim ripgrep stow tmux fd cmake ninka \
+$brew_path/brew install \
+    bat exa fish htop jq neovim ripgrep stow tmux fd cmake ninja \
     pyenv golang node robotsandpencils/made/xcodes
+
+echo $brew_path/fish >> /etc/shells
+chsh -s $brew_path/fish
+
+mkdir -p $HOME/.local/share
+
+export CARGO_HOME=$HOME/.local/share/cargo
+export RUSTUP_HOME=$HOME/.local/share/rustup
+curl -fsSL https://sh.rustup.rs | /bin/sh -s -- -y --no-modify-path
+
+git clone https://github.com/ddeville/base16-shell.git $HOME/.local/share/base16-shell
