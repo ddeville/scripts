34f4de9a15f8c45fac5466e6306c8f5fdd495e4e
Damien DeVille <damien@ddeville.me>
2021-08-12 11:09:52 -0700

    Add keyring and get ssh to use it

--- install/arch/base.sh
+++ install/arch/base.sh
@@ -27,7 +27,7 @@ pacman -S --needed \
     base-devel linux-headers man-db man-pages git pacman-contrib \
     xdg-user-dirs xdg-utils dialog terminus-font \
     networkmanager network-manager-applet libvncserver \
-    openssh rsync openbsd-netcat iptables ipset firewalld gnupg \
+    openssh rsync openbsd-netcat iptables ipset firewalld gnupg gnome-keyring libsecret \
     avahi bluez bluez-utils cups hplip inetutils dnsutils nss-mdns \
     alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol sof-firmware \
     acpi acpi_call acpid \
--- install/arch/x.sh
+++ install/arch/x.sh
@@ -5,7 +5,7 @@ sudo pacman -Syy
 sudo pacman -S --needed \
     xorg xorg-xinit lxappearance \
     bspwm sxhkd slock lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings \
-    dunst redshift neofetch playerctl rofi \
+    dunst redshift neofetch playerctl rofi seahorse \
     alacritty firefox-developer-edition nautilus sxiv mupdf remmina \
     faenza-icon-theme papirus-icon-theme \
     ttf-anonymous-pro ttf-liberation inter-font ttf-dejavu noto-fonts noto-fonts-emoji tex-gyre-fonts gnu-free-fonts xorg-fonts-misc ttf-linux-libertine
