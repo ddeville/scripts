5784026f16de1a4704d3b0ee3664902e47b7ef9a
Damien DeVille <damien@ddeville.me>
2021-08-12 23:20:11 -0700

    New function to move to the nearest git repo root

---
+++ config/common/.config/fish/functions/cdg.fish
@@ -0,0 +1,8 @@
+function cdg --description "Change directory to the nearest parent that is the root of a git repository"
+	while test $PWD != "/"
+		if test -d .git
+			break
+		end
+		cd ..
+	end
+end
