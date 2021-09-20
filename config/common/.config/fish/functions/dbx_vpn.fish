1132bb03c00b85adb01846aaeb0727596417c09a
Damien DeVille <damien@ddeville.me>
2021-09-20 12:41:33 -0700

    Support vpn on non-macos

--- config/common/.config/fish/functions/dbx_vpn.fish
+++ config/common/.config/fish/functions/dbx_vpn.fish
@@ -12,14 +12,18 @@ function dbx_vpn --description "Forward port to Dropbox Mac Pro and set up proxy
         set server "damien-mp.corp.kattungar.net"
     end
 
-    echo "Enabling SOCKS proxy on" $network_service "with port" $port
-    command networksetup -setsocksfirewallproxy $network_service localhost $port
-    command networksetup -setsocksfirewallproxystate $network_service on
+    if [ (uname -s) = "Darwin" ]
+        echo "Enabling SOCKS proxy on" $network_service "with port" $port
+        command networksetup -setsocksfirewallproxy $network_service localhost $port
+        command networksetup -setsocksfirewallproxystate $network_service on
+    end
 
     echo "SSH connecting to" $server
     echo "SSH binding to port" $port
     command ssh -D $port -N $server
 
-    echo "Disabling SOCKS proxy"
-    command networksetup -setsocksfirewallproxystate $network_service off
+    if [ (uname -s) = "Darwin" ]
+        echo "Disabling SOCKS proxy"
+        command networksetup -setsocksfirewallproxystate $network_service off
+    end
 end
