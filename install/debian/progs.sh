7749ca54c426c2440fc5807022dfae41cdf342aa
Damien DeVille <damien@ddeville.me>
2021-10-26 21:54:06 -0700

    Fix name

--- install/debian/progs.sh
+++ install/debian/progs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sudo apt-get install \
-    fish vim neovim stow bat exa htop jq ripgrep tmux curl fd \
+    fish vim neovim stow bat exa htop jq ripgrep tmux curl fd-find \
     cmake ninja python rustup go perf gdb flamegraph
