e5192176aad6ee33df55971c3402b3b0912b8318
Damien DeVille <damien@ddeville.me>
2022-01-22 09:40:48 -0800

    Add the right key

--- config/common/.config/fish/functions/dbx_rdp_win_build.fish
+++ config/common/.config/fish/functions/dbx_rdp_win_build.fish
@@ -1,3 +1,6 @@
 function dbx_rdp_win_build --description "Setup RDP connection to the Windows build machine"
-    ssh -N -L 13389:clientbuild-win01:3389 damien@build-ssh.corp.dropbox.com -J damien-mp.home.kattungar.net
+    if not ssh-add -l 2> /dev/null | grep dbx_id_rsa > /dev/null 2>&1
+        ssh-add ~/.ssh/dbx_id_rsa
+    end
+    ssh -N -L 13389:clientbuild-win01:3389 damien@build-ssh.corp.dropbox.com -J damien@damien-mp.home.kattungar.net
 end
