2954064cffc41de5b957863a498a2c138dceb304
Damien DeVille <damien@ddeville.me>
2022-01-22 10:05:38 -0800

    Specify the identity key manually

--- config/common/.config/fish/functions/dbx_devbox.fish
+++ config/common/.config/fish/functions/dbx_devbox.fish
@@ -1,9 +1,5 @@
 function dbx_devbox --description "SSH to my devbox"
-    if not ssh-add -l 2> /dev/null | grep dbx_id_rsa > /dev/null 2>&1
-        ssh-add ~/.ssh/dbx_id_rsa
-    end
-
-    ssh -oServerAliveInterval=180 -oServerAliveCountMax=2 -A -J\
+    ssh -i ~/.ssh/dbx_id_rsa -oServerAliveInterval=180 -oServerAliveCountMax=2 -A -J\
         damien@damien-mp.home.kattungar.net\
         damien-dbx -t "
         if not tmux list-sessions 2> /dev/null | grep devbox > /dev/null 2>&1
--- config/common/.config/fish/functions/dbx_rdp_win_build.fish
+++ config/common/.config/fish/functions/dbx_rdp_win_build.fish
@@ -1,6 +1,3 @@
 function dbx_rdp_win_build --description "Setup RDP connection to the Windows build machine"
-    if not ssh-add -l 2> /dev/null | grep dbx_id_rsa > /dev/null 2>&1
-        ssh-add ~/.ssh/dbx_id_rsa
-    end
-    ssh -N -L 13389:clientbuild-win01:3389 damien@build-ssh.corp.dropbox.com -J damien@damien-mp.home.kattungar.net
+    ssh -i ~/.ssh/dbx_id_rsa -N -L 13389:clientbuild-win01:3389 damien@build-ssh.corp.dropbox.com -J damien@damien-mp.home.kattungar.net
 end
