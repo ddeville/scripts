8dbdd2433cf730ffe281111b172595008f9bee30
Damien DeVille <damien@ddeville.me>
2022-05-13 19:56:40 -0700

    Update win devbox machine name

--- config/common/.config/fish/functions/dbx_rdp_win_devbox.fish
+++ config/common/.config/fish/functions/dbx_rdp_win_devbox.fish
@@ -1,3 +1,3 @@
 function dbx_rdp_win_devbox --description "Setup RDP connection to my Windows devbox"
-    ssh -N -L 23389:damien-win:3389 damien@damien-studio.home.kattungar.net
+    ssh -N -L 23389:damien-win1:3389 damien@damien-studio.home.kattungar.net
 end
