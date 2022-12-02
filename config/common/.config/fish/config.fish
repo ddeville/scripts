ff4d4673b2c71b5c044daa4b70dae7e7e5f3f658
Damien Deville <damien@ddeville.me>
2022-12-02 15:11:53 -0800

    Actually it needs to be xterm :/

--- config/common/.config/fish/config.fish
+++ config/common/.config/fish/config.fish
@@ -3,11 +3,7 @@ set -gx EDITOR nvim
 set -gx PYENV_SHELL fish
 
 # We need this to be set for git
-if test -n "$TMUX"
-    set -gx TERM tmux-256color
-else
-    set -gx TERM xterm-256color
-end
+set -gx TERM xterm-256color
 
 set -l base16_path "$XDG_DATA_HOME/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
