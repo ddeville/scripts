49ead2c2551a141b66d3db3f9807d98ba7228f4d
Damien Deville <damien@ddeville.me>
2022-12-02 15:10:48 -0800

    Fix missing TERM env var for git

--- config/common/.config/fish/config.fish
+++ config/common/.config/fish/config.fish
@@ -2,6 +2,13 @@ set -gx CLICOLOR 1
 set -gx EDITOR nvim
 set -gx PYENV_SHELL fish
 
+# We need this to be set for git
+if test -n "$TMUX"
+    set -gx TERM tmux-256color
+else
+    set -gx TERM xterm-256color
+end
+
 set -l base16_path "$XDG_DATA_HOME/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
     source $base16_path
