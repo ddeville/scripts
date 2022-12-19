53d9d49a1965db760187759d6ba93e3b20d01cf7
Damien Deville <damien@ddeville.me>
2022-12-18 18:14:55 -0800

    Search hidden files

--- config/common/.config/nvim/after/plugin/vim-grepper.lua
+++ config/common/.config/nvim/after/plugin/vim-grepper.lua
@@ -1,6 +1,9 @@
 vim.g.grepper = {
   tools = { "rg", "git", "grep" };
   highlight = 1;
+  rg = {
+    grepprg = "rg -H --no-heading --hidden --vimgrep";
+  };
 }
 
 vim.keymap.set("n", "<leader>f", ":Grepper -tool rg<CR>", { noremap = true, silent = true })
