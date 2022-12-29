ecee9c4847e14741c07872358d7028b45d3f469b
Damien Deville <damien@ddeville.me>
2022-12-28 23:17:52 -0800

    Fix issue finding system headers when only CLI tools are installed

--- config/common/.config/fish/config.fish
+++ config/common/.config/fish/config.fish
@@ -15,3 +15,8 @@ if test -e "$HOME/src/server/go"
     # For Dropbox server code
     set -x GOPATH $GOPATH "$HOME/src/server/go"
 end
+
+# Sometimes this is not set correctly if only the Xcode CLI tools are installed
+if test (uname) = "Darwin" && ! test -n "$SDKROOT"
+    set -gx SDKROOT (xcrun --sdk macosx --show-sdk-path)
+end
