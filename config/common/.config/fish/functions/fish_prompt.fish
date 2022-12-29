64f2f274d3961cf96d33efeb0571c25b3fa1b524
Damien Deville <damien@ddeville.me>
2022-12-28 19:04:41 -0800

    Show venv in fish prompt

--- config/common/.config/fish/functions/fish_prompt.fish
+++ config/common/.config/fish/functions/fish_prompt.fish
@@ -9,11 +9,16 @@ function fish_prompt
         function _git_branch_name
             echo (git symbolic-ref HEAD 2>/dev/null | sed -e "s|^refs/heads/||")
         end
+
+        function _pyenv_virtualenv_name
+            echo (basename "$VIRTUAL_ENV")
+        end
     end
 
     set -l cwd_color (set_color $fish_color_cwd)
-    set -l yellow (set_color yellow)
-    set -l red (set_color red)
+    set -l host_color (set_color yellow)
+    set -l branch_color (set_color red)
+    set -l venv_color (set_color cyan)
     set -l normal_color (set_color $fish_color_normal)
 
     if test "$USER" = "root"
@@ -23,14 +28,20 @@ function fish_prompt
     end
 
     set -l cwd_ $cwd_color(basename (prompt_pwd))
-    set -l hostname_ $yellow(hostname -s)
+    set -l hostname_ $host_color(hostname -s)
+
+    if set -q VIRTUAL_ENV
+        set -l venv_name $venv_color(_pyenv_virtualenv_name)
+        set venv_info "$normal_color:[$venv_name]$normal_color"
+
+    end
 
     if test (_is_git_repo)
-        set -l repo_branch $red(_git_branch_name)
+        set -l repo_branch $branch_color(_git_branch_name)
         set repo_info "$normal_color:($repo_branch$normal_color)"
     end
 
-    echo -n -s $hostname_$normal_color:$cwd_ $repo_info $normal_color" "$normal_color$prompt_sign" "
+    echo -n -s $hostname_$normal_color:$cwd_ $venv_info $repo_info $normal_color" "$normal_color$prompt_sign" "
 
     set_color normal
 end
