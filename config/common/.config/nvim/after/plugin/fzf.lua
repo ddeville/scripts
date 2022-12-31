37e3cb8878ccbfbcf2d63f0bf3cef0f747db97df
Damien Deville <damien@ddeville.me>
2022-12-31 13:58:42 -0800

    Switch to Telescope for file and buffer search

--- config/common/.config/nvim/after/plugin/fzf.lua
+++ config/common/.config/nvim/after/plugin/fzf.lua
@@ -2,11 +2,3 @@ vim.g.fzf_layout = { down = "35%" }
 
 -- we use different commands in the shell
 vim.env.FZF_DEFAULT_COMMAND = "fd --type file --color=never --hidden --strip-cwd-prefix --exclude .git"
-
-vim.keymap.set("n", "<leader>;", ":Buffers<CR>")
-vim.keymap.set("n", ";", ":Buffers<CR>")
-vim.keymap.set("n", "<leader>o", ":Files<CR>")
-vim.keymap.set("n", "<C-p>", ":Files<CR>")
-vim.keymap.set("n", "<leader>s", ":GFiles?<CR>")
-vim.keymap.set("n", "<leader>t", ":Tags<CR>")
-vim.keymap.set("n", "//", ":BLines<CR>")
--- config/common/.config/nvim/after/plugin/nvim-telescope.lua
+++ config/common/.config/nvim/after/plugin/nvim-telescope.lua
@@ -1,13 +1,66 @@
 local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
+
+vim.keymap.set("n", "<leader>;", builtin.buffers)
+vim.keymap.set("n", ";", builtin.buffers)
+vim.keymap.set("n", "<leader>o", builtin.find_files)
+vim.keymap.set("n", "<C-p>", builtin.find_files)
+vim.keymap.set("n", "<leader>s", builtin.git_status)
+vim.keymap.set("n", "<leader>t", builtin.tags)
+vim.keymap.set("n", "//", builtin.current_buffer_fuzzy_find)
 
 telescope.setup {
+  defaults = {
+    mappings = {
+      -- Close when hitting escape in insert mode, rather than going to normal mode
+      i = {
+        ["<esc>"] = actions.close,
+      };
+    };
+  };
+  pickers = {
+    buffers = {
+      theme = "ivy";
+      results_title = false;
+      preview_title = false;
+      sorting_strategy = "descending";
+      sort_lastused = true;
+      layout_config = {
+        height = 0.35;
+        prompt_position = "bottom";
+        preview_width = 0.45;
+      };
+      mappings = {
+        i = {
+          ["<c-d>"] = actions.delete_buffer;
+        };
+        n = {
+          ["<c-d>"] = actions.delete_buffer;
+        };
+      };
+    };
+    find_files = {
+      theme = "ivy";
+      results_title = false;
+      preview_title = false;
+      find_command = { "fd", "--type", "file", "--color=never", "--hidden", "--strip-cwd-prefix", "--exclude", ".git" };
+      sorting_strategy = "descending";
+      layout_config = {
+        height = 0.45;
+        prompt_position = "bottom";
+        preview_width = 0.45;
+      };
+    };
+  };
   extensions = {
     fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    }
-  }
+      fuzzy = true;
+      override_generic_sorter = true;
+      override_file_sorter = true;
+      case_mode = "smart_case";
+    };
+  };
 }
+
 telescope.load_extension("fzf")
