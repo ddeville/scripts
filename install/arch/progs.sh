5fe780e99def321ee66ac330968d64f943ec432d
Damien Deville <damien@ddeville.me>
2023-01-01 15:59:50 -0800

    Install stylua

--- install/arch/progs.sh
+++ install/arch/progs.sh
@@ -4,4 +4,4 @@ set -eu
 paru -S --needed \
     dropbox nautilus-dropbox dropbox-cli \
     google-chrome 1password 1password-cli spotify slack discord zoom \
-    duplicity sysz flamegraph pyenv-virtualenv
+    duplicity sysz flamegraph pyenv-virtualenv stylua
