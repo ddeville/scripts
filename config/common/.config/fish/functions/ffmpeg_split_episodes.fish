543c0f59e4b4862349750c33cf5c5dcc9cf74c22
Damien Deville <damien@ddeville.me>
2023-01-07 11:18:32 -0800

    Check for status

--- config/common/.config/fish/functions/ffmpeg_split_episodes.fish
+++ config/common/.config/fish/functions/ffmpeg_split_episodes.fish
@@ -2,10 +2,6 @@ function ffmpeg_split_episodes
     argparse --name=ffmpeg_split_episodes 'n/name_format=' 't/timestamp=' 'c/count=' -- $argv
     or return
 
-    echo $_flag_name_format
-    echo $_flag_timestamp
-    echo $_flag_count
-
     if test -d $_flag_name_format; or test -d $_flag_timestamp; or test -d $_flag_count
         echo "Usage: "
         echo "  ffmpeg_split_episodes Curious.George.S01E%s.1080p.WEB.h264-NOMA.mkv \"00:11:58\" 30"
@@ -13,11 +9,18 @@ function ffmpeg_split_episodes
     end
 
     for idx in (seq -f "%02g" $_flag_count)
-        set num1 (printf "%02d" (math "$idx * 2 - 1"))
-        set num2 (printf "%02d" (math "$idx * 2"))
         set filename (printf $_flag_name_format $idx)
-        set extension (string split -r -m1 . $filename)[2]
+        set ext (string split -r -m1 . $filename)[2]
+        set ts $_flag_timestamp
 
-        ffmpeg -t $_flag_timestamp -c copy $num1.$extension -ss $_flag_timestamp -c copy $num2.$extension -i $filename
+        set out1 (printf "%02d.$ext" (math "$idx * 2 - 1"))
+        set out2 (printf "%02d.$ext" (math "$idx * 2"))
+
+        ffmpeg -t "$ts" -c copy "$out1" -ss "$ts" -c copy "$out2" -i "$filename"
+
+        if test $status -ne 0
+            echo "Failed to run ffmpeg, stopping"
+            return $status
+        end
     end
 end
