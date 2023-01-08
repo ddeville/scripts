bbc844ca10b637d7cb730212640b61d59b14d850
Damien Deville <damien@ddeville.me>
2023-01-07 16:59:53 -0800

    Stop overriding TERM, instead let's work on fixing the terminfo on macos

--- config/common/.config/fish/config.fish
+++ config/common/.config/fish/config.fish
@@ -2,9 +2,6 @@ set -gx CLICOLOR 1
 set -gx EDITOR nvim
 set -gx PYENV_SHELL fish
 
-# We need this to be set for git
-set -gx TERM xterm-256color
-
 set -l base16_path "$XDG_DATA_HOME/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
     source $base16_path
