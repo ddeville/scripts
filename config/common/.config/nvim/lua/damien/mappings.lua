44a264c0c68006e53495f9e5af44e0aa43db687d
Damien Deville <damien@ddeville.me>
2023-01-07 18:59:12 -0800

    Integration with system clipboard in nvim

--- config/common/.config/nvim/lua/damien/mappings.lua
+++ config/common/.config/nvim/lua/damien/mappings.lua
@@ -38,3 +38,11 @@ vim.keymap.set('n', 'Y', 'y$')
 
 -- don't clobber the yank register when deleting with x
 vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
+
+-- integration with system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
