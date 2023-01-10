caf4dedf6f0cb8bc74e010148333434290e3bf48
Damien Deville <damien@ddeville.me>
2023-01-09 23:45:41 -0800

    More shfmt

--- config/common/.ssh/use_1password.sh
+++ config/common/.ssh/use_1password.sh
@@ -26,24 +26,24 @@ ssh_ver=$(ssh -V 2>&1 | sed 's/,.*//; s/^OpenSSH_//; s/[^0123456789.].*//')
 ssh_major=$(echo $ssh_ver | awk -F'.' '{printf $1}')
 ssh_minor=$(echo $ssh_ver | awk -F'.' '{printf $2}')
 if [ "$ssh_major" -lt "7" ]; then
-    exit 1
+  exit 1
 elif [ "$ssh_major" -eq "7" ] && [ "$ssh_minor" -lt "3" ]; then
-    exit 1
+  exit 1
 fi
 
 # If we're over an active SSH connection we shouldn't use the local 1p agent.
 if [ -n "$SSH_CONNECTION" ]; then
-    exit 1
+  exit 1
 fi
 
 # If we're in tmux and attached from a shell that has an active SSH connection we shouldn't use the local 1p agent.
 if [ -n "$TMUX" ]; then
-    # `tmux show-environment SSH_CONNECTION` outputs the following:
-    #   - `-SSH_CONNECTION` when the variable is not set
-    #   - `SSH_CONNECTION=192.168.101.24 49370 192.168.101.26 22` when it is set
-    if [[ $(tmux show-environment SSH_CONNECTION) == *=* ]]; then
-        exit 1
-    fi
+  # `tmux show-environment SSH_CONNECTION` outputs the following:
+  #   - `-SSH_CONNECTION` when the variable is not set
+  #   - `SSH_CONNECTION=192.168.101.24 49370 192.168.101.26 22` when it is set
+  if [[ $(tmux show-environment SSH_CONNECTION) == *=* ]]; then
+    exit 1
+  fi
 fi
 
 # Only use 1p if the socket exists (either as a socket or a symlink to one).
