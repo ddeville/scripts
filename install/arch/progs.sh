bc9df79eba4f22c23538d65ee21cd1ad5d742f01
Damien Deville <damien@ddeville.me>
2023-01-09 22:33:56 -0800

    Install shellcheck

--- install/arch/progs.sh
+++ install/arch/progs.sh
@@ -4,4 +4,5 @@ set -eu
 paru -S --needed \
     dropbox nautilus-dropbox dropbox-cli \
     google-chrome 1password 1password-cli spotify slack discord zoom \
-    duplicity sysz flamegraph pyenv-virtualenv stylua
+    duplicity sysz flamegraph pyenv-virtualenv \
+    stylua shellcheck
