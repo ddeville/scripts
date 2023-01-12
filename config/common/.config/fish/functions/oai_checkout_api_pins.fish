810234a5856482671bf8f86136d8c11227982482
Damien Deville <damien@ddeville.me>
2023-01-12 13:51:32 -0800

    Cleanup

--- config/common/.config/fish/functions/oai_checkout_api_pins.fish
+++ config/common/.config/fish/functions/oai_checkout_api_pins.fish
@@ -1,6 +1,5 @@
 function oai_checkout_api_pins --description "Checkout whatever is the current version for pinned repos in API"
     set -l code_dir "$HOME/code"
-    set -l manage_dir "$code_dir/api/manage"
 
     pushd "$code_dir/model-runner-api"
     git fetch && git checkout (cat "$manage_dir/model-runner-api.version")
