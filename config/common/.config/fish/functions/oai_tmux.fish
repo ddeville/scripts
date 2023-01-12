2a5d525c00beae951181b3c50a2a6d0fd2dea568
Damien Deville <damien@ddeville.me>
2023-01-12 13:51:36 -0800

    Also open py module repo

--- config/common/.config/fish/functions/oai_tmux.fish
+++ config/common/.config/fish/functions/oai_tmux.fish
@@ -21,6 +21,9 @@ function oai_tmux --description "Create OAI tmux session"
         command tmux new-window -t $session -n notes -d -c ~/Desktop
         command tmux send-keys -t $session:6 "nvim notes.txt" Enter
 
+        # openai-python
+        command tmux new-window -t $session -n openai-py -d -c ~/code/openai-python
+
         # let's now kill the original window and move them all back by 1
         command tmux kill-window -t $session:1
         command tmux move-window -s $session:2 -t $session:1
@@ -28,6 +31,7 @@ function oai_tmux --description "Create OAI tmux session"
         command tmux move-window -s $session:4 -t $session:3
         command tmux move-window -s $session:5 -t $session:4
         command tmux move-window -s $session:6 -t $session:5
+        command tmux move-window -s $session:7 -t $session:6
 
         # and select the first one
         command tmux select-window -t $session:1
