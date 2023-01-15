0eb4ab1fed11dbc4009f10b19b2be0df7fabd7e0
Damien Deville <damien@ddeville.me>
2023-01-14 23:53:23 -0800

    Setup base16 in fish

--- config/common/.config/fish/conf.d/00_xdg.fish
+++ config/common/.config/fish/conf.d/00_xdg.fish
@@ -24,22 +24,33 @@ command mkdir -p $XDG_STATE_HOME
 # python
 set -x PYENV_ROOT "$XDG_DATA_HOME/pyenv"
 set -x PYTHONSTARTUP "$XDG_CONFIG_HOME/python/startup"
+
 # rust
 set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -x RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+
 # node
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/config"
 set -x NPM_CONFIG_CACHE "$XDG_CACHE_HOME/npm"
 set -x NODE_REPL_HISTORY "$XDG_STATE_HOME/node/history"
 command mkdir -p "$XDG_STATE_HOME/node"
+
 # less
 set -x LESSKEY "$XDG_CONFIG_HOME/less/lesskey"
 set -x LESSHISTFILE "$XDG_STATE_HOME/less/history"
 command mkdir -p "$XDG_STATE_HOME/less"
+
 # bash
 set -x HISTFILE "$XDG_STATE_HOME/bash/history"
 command mkdir -p "$XDG_STATE_HOME/bash"
+
 # base16
 set -x BASE16_CONFIG_PATH "$XDG_STATE_HOME/base16"
+set -x BASE16_SHELL_PATH "$XDG_DATA_HOME/base16/base16-shell"
+set -x BASE16_SHELL_HOOKS_PATH "$XDG_CONFIG_HOME/base16/hooks"
+
 # ripgrep
 set -x RIPGREP_CONFIG_PATH "$XDG_CONFIG_HOME/ripgrep/ripgreprc"
+
+# golang
+set -x GOPATH "$HOME/src/go"
