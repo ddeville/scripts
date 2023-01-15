5db1ef5407bd9bfd2f0438e16da42efcc63dc8d5
Damien Deville <damien@ddeville.me>
2023-01-15 14:06:07 -0800

    Use the 256 version of the theme

--- config/common/.config/base16/hooks/alacritty.fish
+++ config/common/.config/base16/hooks/alacritty.fish
@@ -2,4 +2,4 @@
 
 read current_theme_name <"$BASE16_SHELL_THEME_NAME_PATH"
 
-ln -sf "$XDG_DATA_HOME/base16/base16-alacritty/colors/base16-$current_theme_name.yml" "$BASE16_CONFIG_PATH/base16-alacritty.yml"
+ln -sf "$XDG_DATA_HOME/base16/base16-alacritty/colors/base16-$current_theme_name-256.yml" "$BASE16_CONFIG_PATH/base16-alacritty.yml"
