508ae3f9fbfa4f6a90d8ed57c328269132b035a2
Damien Deville <damien@ddeville.me>
2023-01-15 00:47:22 -0800

    Format

--- config/common/.config/base16/hooks/nvim.fish
+++ config/common/.config/base16/hooks/nvim.fish
@@ -1,8 +1,8 @@
 # This script is called whenever the base16 theme is loaded or changed.
 
-set nvim_path "$BASE16_CONFIG_PATH/set_theme.lua"
 read current_theme_name <"$BASE16_SHELL_THEME_NAME_PATH"
 
+set nvim_path "$BASE16_CONFIG_PATH/set_theme.lua"
 set nvim_output (string join "\n" \
   "local current_theme_name = \"$current_theme_name\"" \
   "if current_theme_name ~= \"\" and vim.g.colors_name ~= 'base16-' .. current_theme_name then" \
