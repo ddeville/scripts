5a147fa9d3fbb1130974cc5486649c04b11ef44d
Damien Deville <damien@ddeville.me>
2023-01-14 23:20:40 -0800

    Set default base16 theme

--- config/common/.config/fish/config.fish
+++ config/common/.config/fish/config.fish
@@ -2,11 +2,14 @@ set -gx CLICOLOR 1
 set -gx EDITOR nvim
 set -gx PYENV_SHELL fish
 
+# Base16
 set -l base16_path "$XDG_DATA_HOME/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
     source $base16_path
 end
+set -x BASE16_THEME_DEFAULT base16-gruvbox-dark-pale
 
+# Golang
 set -x GOPATH "$HOME/src/go"
 
 # setup `fzf`
