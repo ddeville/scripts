21bd774ef10d88cd3d89498666c8369907b14b52
Damien Deville <damien@ddeville.me>
2023-01-15 14:03:54 -0800

    Load alacritty colors from base16 hook

---
+++ config/common/.config/base16/hooks/alacritty.fish
@@ -0,0 +1,5 @@
+# This script is called whenever the base16 theme is loaded or changed.
+
+read current_theme_name <"$BASE16_SHELL_THEME_NAME_PATH"
+
+ln -sf "$XDG_DATA_HOME/base16/base16-alacritty/colors/base16-$current_theme_name.yml" "$BASE16_CONFIG_PATH/base16-alacritty.yml"
