260e86d0c700c6c9a0ce99d95856e396c4024a51
Damien Deville <damien@ddeville.me>
2023-01-15 00:46:52 -0800

    Make sure that the default theme is set before we source base16-shell

--- config/common/.config/fish/conf.d/base16.fish
+++ config/common/.config/fish/conf.d/base16.fish
@@ -1,6 +1,6 @@
+set -x BASE16_THEME_DEFAULT gruvbox-dark-pale
+
 set -l base16_path "$XDG_DATA_HOME/base16/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
     source $base16_path
 end
-
-set -x BASE16_THEME_DEFAULT gruvbox-dark-pale
