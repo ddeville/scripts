142002b7e2a5f0cc6f8a90680d11c7266ddbfedf
Damien Deville <damien@ddeville.me>
2023-01-15 11:09:15 -0800

    Support base16 color in fzf

--- config/common/.config/fish/conf.d/fzf.fish
+++ config/common/.config/fish/conf.d/fzf.fish
@@ -1,8 +1,9 @@
-set -x FZF_TMUX 0
-set -x FZF_DEFAULT_OPTS "--height 40% --border --tabstop=4"
-set -x FZF_DEFAULT_COMMAND "fd --exclude .git --hidden --color=never"
-set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-set -x FZF_ALT_C_COMMAND "fd --type d --exclude .git --hidden --color=never"
+set -gx FZF_TMUX 0
+set -gx FZF_DEFAULT_COMMAND "fd --exclude .git --hidden --color=never"
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_ALT_C_COMMAND "fd --type d --exclude .git --hidden --color=never"
+set -gx FZF_NON_COLOR_OPTS "--height 40% --border --tabstop=4"
+set -gx FZF_DEFAULT_OPTS "$FZF_NON_COLOR_OPTS $FZF_BASE16_COLOR_OPS"
 
 # alt-c doesn't work on macos so rather than tweaking the iterm settings bind it
 bind -M insert ç fzf-cd-widget
