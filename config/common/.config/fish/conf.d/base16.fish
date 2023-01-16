2816b7334a98038c719587b56052d3fc4d93ca70
Damien Deville <damien@ddeville.me>
2023-01-16 09:06:41 -0800

    Add todo

--- config/common/.config/fish/conf.d/base16.fish
+++ config/common/.config/fish/conf.d/base16.fish
@@ -2,5 +2,6 @@ set -x BASE16_THEME_DEFAULT gruvbox-dark-pale
 
 set -l base16_path "$XDG_DATA_HOME/base16/base16-shell/profile_helper.fish"
 if status --is-interactive && test -e $base16_path
+    # TODO(damien): Profile what is slow to run in this script
     source $base16_path
 end
