182afc523d78fb649a3b1162806a9729bec90de9
Damien Deville <damien@ddeville.me>
2023-01-16 18:34:31 -0800

    Don't read if the file doesn't exist

--- config/common/.config/base16/hooks/nvim.fish
+++ config/common/.config/base16/hooks/nvim.fish
@@ -1,7 +1,9 @@
 # This script is called whenever the base16 theme is loaded or changed.
 
 read current_theme_name <"$BASE16_SHELL_THEME_NAME_PATH"
-read previous_theme_name <"$BASE16_SHELL_PREVIOUS_THEME_NAME_PATH"
+if test -e "$BASE16_SHELL_PREVIOUS_THEME_NAME_PATH"
+    read previous_theme_name <"$BASE16_SHELL_PREVIOUS_THEME_NAME_PATH"
+end
 
 if test "$current_theme_name" != "$previous_theme_name"
     set nvim_path "$BASE16_CONFIG_PATH/set_theme.lua"
