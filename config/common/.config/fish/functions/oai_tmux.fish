638ab0f337834e988006249c7386fbe4fa54499d
Damien Deville <damien@ddeville.me>
2023-01-18 17:34:48 -0800

    Create different sessions

--- config/common/.config/fish/functions/oai_tmux.fish
+++ config/common/.config/fish/functions/oai_tmux.fish
@@ -1,40 +1,41 @@
-function oai_tmux --description "Create OAI tmux session"
-    set session oai
-    if not command tmux list-sessions 2>/dev/null | grep $session >/dev/null 2>&1
-        # create the session at ~
-        command tmux new-session -D -s $session -d -c ~
-
-        # api
-        command tmux new-window -t $session -n api -d -c ~/code/api
-
-        # editor
-        command tmux new-window -t $session -n editor -d -c ~/code/api
-        command tmux send-keys -t $session:3 "nvim ." Enter
-
-        # tf
-        command tmux new-window -t $session -n tf -d -c ~/code/terraform-config-api
-
-        # scripts
-        command tmux new-window -t $session -n scripts -d -c ~/scripts
-
-        # notes
-        command tmux new-window -t $session -n notes -d -c ~/Desktop
-        command tmux send-keys -t $session:6 "nvim notes.txt" Enter
-
-        # openai-python
-        command tmux new-window -t $session -n openai-py -d -c ~/code/openai-python
-
-        # let's now kill the original window and move them all back by 1
-        command tmux kill-window -t $session:1
-        command tmux move-window -s $session:2 -t $session:1
-        command tmux move-window -s $session:3 -t $session:2
-        command tmux move-window -s $session:4 -t $session:3
-        command tmux move-window -s $session:5 -t $session:4
-        command tmux move-window -s $session:6 -t $session:5
-        command tmux move-window -s $session:7 -t $session:6
-
-        # and select the first one
-        command tmux select-window -t $session:1
+function oai_tmux --description "Create OAI tmux sessions"
+    if test -n "$TMUX"
+        echo "You cannot create tmux sessions inside of tmux."
+        return 1
     end
-    command tmux attach -d -t $session
+    if command tmux list-sessions >/dev/null 2>&1
+        echo "There are some existing sessions in tmux, first kill them."
+        return 1
+    end
+
+    # The API session
+    command tmux new-session -D -s api -d -c ~/code/api
+    command tmux rename-window -t api:1 repo
+    command tmux new-window -t api -n editor -d -c ~/code/api
+    command tmux send-keys -t api:2 "nvim ." Enter
+
+    # The openai-py session
+    command tmux new-session -D -s openai-py -d -c ~/code/openai-python
+    command tmux rename-window -t openai-py:1 repo
+    command tmux new-window -t openai-py -n editor -d -c ~/code/openai-python
+    command tmux send-keys -t openai-py:2 "nvim ." Enter
+
+    # The Terraform session
+    command tmux new-session -D -s terraform -d -c ~/code/terraform-config-api
+    command tmux rename-window -t terraform:1 repo
+    command tmux new-window -t terraform -n editor -d -c ~/code/terraform-config-api
+    command tmux send-keys -t terraform:2 "nvim ." Enter
+
+    # The scripts and notes session
+    command tmux new-session -D -s scripts -d -c ~/scripts
+    command tmux rename-window -t scripts:1 repo
+    command tmux new-window -t scripts -n editor -d -c ~/scripts
+    command tmux send-keys -t scripts:2 "nvim ." Enter
+    command tmux new-window -t scripts -n notes -d -c ~/Desktop
+    command tmux send-keys -t scripts:3 "nvim notes.txt" Enter
+
+    # Select the API session
+    command tmux switch-client -t api
+    command tmux select-window -t api:1
+    command tmux attach -d -t api
 end
