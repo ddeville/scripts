e05d6b2ffaf00f7f49512b9c5aa79dfc502b3af7
Damien Deville <damien@ddeville.me>
2023-01-31 10:37:22 -0800

    New function to check disk space on build boxes

---
+++ config/common/.config/fish/functions/oai_buildbox_check_space.fish
@@ -0,0 +1,12 @@
+function oai_buildbox_check_space --description "Check the remaining spaces on build boxes"
+    for box in a b c d e f g h i j
+        set df (az ssh vm --ip "cpu-$box.build-box.internal.api.openai.org" -- -o PubkeyAcceptedKeyTypes=+ssh-rsa-cert-v01@openssh.com "df -h" 2>/dev/null | grep /mnt)
+        set comps (string split --no-empty ' ' $df)
+        echo "cpu-$box is using $comps[5] ($comps[3] out of $comps[4])"
+    end
+    for box in a b c
+        set df (az ssh vm --ip "gpu-$box.build-box.internal.api.openai.org" -- -o PubkeyAcceptedKeyTypes=+ssh-rsa-cert-v01@openssh.com "df -h" 2>/dev/null | grep /mnt)
+        set comps (string split --no-empty ' ' $df)
+        echo "gpu-$box is using $comps[5] ($comps[3] out of $comps[4])"
+    end
+end
