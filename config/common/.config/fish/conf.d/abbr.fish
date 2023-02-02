b23ab8772a1ebb067275d866d3384d731dd3378a
Damien Deville <damien@ddeville.me>
2023-02-02 09:53:38 -0800

    Not working on a huge monorepo anymore, yay!

--- config/common/.config/fish/conf.d/abbr.fish
+++ config/common/.config/fish/conf.d/abbr.fish
@@ -4,9 +4,9 @@ alias realvim (which vim)
 alias vim nvim
 alias ll "exa --long --all"
 alias tree "exa --tree --level=3"
-alias gaa "git add .; and git ci"
-alias gaaa "git add .; and git ci --amend --no-edit"
-alias gaau "git add .; and git cu"
+alias gaa "git add -A; and git ci"
+alias gaaa "git add -A; and git ci --amend --no-edit"
+alias gaau "git add -A; and git cu"
 alias oo "open ."
 alias tf terraform
 
