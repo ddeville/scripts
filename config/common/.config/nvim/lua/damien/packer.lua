d432bfd91ca0e32265c88a4e89237ad8f105c05b
Damien Deville <damien@ddeville.me>
2023-02-07 15:22:31 -0800

    Install copilot

--- config/common/.config/nvim/lua/damien/packer.lua
+++ config/common/.config/nvim/lua/damien/packer.lua
@@ -97,6 +97,11 @@ packer.startup({
       run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     })
 
+    -- Magic
+    use({
+      'github/copilot.vim',
+    })
+
     -- tpope
     use({
       'tpope/vim-commentary',
