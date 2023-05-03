54e852b0c3f189d873c27dd0ff66ff9143c2ddb5
Damien Deville <damien@ddeville.me>
2023-05-03 14:48:34 -0700

    New repos

--- config/common/.config/fish/functions/oai_checkout_api_pins.fish
+++ config/common/.config/fish/functions/oai_checkout_api_pins.fish
@@ -6,11 +6,19 @@ function oai_checkout_api_pins --description "Checkout whatever is the current v
     git fetch && git checkout (cat "$manage_dir/model-runner-api.version")
     popd
 
+    pushd "$code_dir/dalle-runner-api"
+    git fetch && git checkout (cat "$manage_dir/dalle-runner-api.version")
+    popd
+
     pushd "$code_dir/openai-python"
     git fetch && git checkout (cat "$manage_dir/openai-python.version")
     popd
 
     pushd "$code_dir/whisper-internal"
     git fetch && git checkout (cat "$manage_dir/whisper-internal.version")
+    popd
+
+    pushd "$code_dir/tts-internal"
+    git fetch && git checkout (cat "$manage_dir/tts-internal.version")
     popd
 end
