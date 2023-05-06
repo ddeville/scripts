8916a9bfd8433013bdeba5f5abe212ba4775d21d
Damien Deville <damien@ddeville.me>
2023-05-06 10:35:06 -0700

    Shell format

--- install/arch/install_packages.sh
+++ install/arch/install_packages.sh
@@ -5,11 +5,11 @@ set -eu
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 # Install all the base packages
-readarray -t base_packages < <(grep -Ev "^#|^\$" "$script_dir/base_packages.txt")
+readarray -t base_packages < <(grep -Ev "^#|^$" "$script_dir/base_packages.txt")
 sudo pacman -Syy
 sudo pacman -S --needed "${base_packages[@]}"
 
 # Install AUR packages with Paru
-readarray -t aur_packages < <(grep -Ev "^#|^\$" "$script_dir/aur_packages.txt")
+readarray -t aur_packages < <(grep -Ev "^#|^$" "$script_dir/aur_packages.txt")
 /usr/bin/paru -Syy
 /usr/bin/paru -S --needed "${aur_packages[@]}"
