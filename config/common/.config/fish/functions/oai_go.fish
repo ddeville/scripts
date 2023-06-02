ab0f8c59f908435a81bc2aa6981850954895c955
Damien Deville <damien@ddeville.me>
2023-06-01 21:51:38 -0700

    delete a couple of functions

--- config/common/.config/fish/functions/oai_go.fish
+++
@@ -1,3 +0,0 @@
-function oai_go
-    PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig" go $argv -tags dynamic
-end
--- config/common/.config/fish/functions/oai_nuke_ci_agent_mypy_cache.fish
+++
@@ -1,5 +0,0 @@
-function oai_nuke_ci_agent_mypy_cache --description "Delete the cache on all the ci-agent pods"
-    set ctx internal-ci-api
-    set label "app=ci-agent"
-    kubectl --context $ctx get pods -o name -l $label | xargs -I{} kubectl --context $ctx exec {} -- /bin/bash -c "rm -rf /tmp/mypy-cache"
-end
