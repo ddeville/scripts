1be024a32251f34f2e4544f8c5dd4c81cec2a59b
Damien Deville <damien@ddeville.me>
2023-06-02 15:08:48 -0700

    add applied completions

--- config/common/.config/fish/conf.d/oai.fish
+++ config/common/.config/fish/conf.d/oai.fish
@@ -68,3 +68,25 @@ end
 alias a applied
 alias appy applied
 alias ak "applied kubectl"
+
+function _applied_completion
+    set -l response
+
+    for value in (env _APPLIED_COMPLETE=fish_complete COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) applied)
+        set response $response $value
+    end
+
+    for completion in $response
+        set -l metadata (string split "," $completion)
+
+        if test $metadata[1] = dir
+            __fish_complete_directories $metadata[2]
+        else if test $metadata[1] = file
+            __fish_complete_path $metadata[2]
+        else if test $metadata[1] = plain
+            echo $metadata[2]
+        end
+    end
+end
+
+complete --no-files --command applied --arguments "(_applied_completion)"
