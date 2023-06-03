ac03da09384a180141f4611496978a389961fdea
Damien Deville <damien@ddeville.me>
2023-06-03 14:22:56 -0700

    Remove mapping of aliases from bash script

--- config/common/.config/fish/conf.d/oai.fish
+++ config/common/.config/fish/conf.d/oai.fish
@@ -20,38 +20,6 @@ function acrlogin
     az acr login -n openaiapiglobal --subscription api
 end
 
-# This function is used by the aliases created below
-function nicer_kubectl
-    set cluster $argv[1]
-    if test (count $argv) -eq 1
-        if test $COLUMNS -gt 200
-            kubectl --context="$cluster" get pods -o wide
-        else
-            kubectl --context="$cluster" get pods
-        end
-    else
-        kubectl --context="$cluster" $argv[2..-1]
-    end
-end
-
-set -l oia_script_path "$API_REPO_PATH/devtools/bootstrap/bash_or_zsh_rc"
-
-# Read all the aliases from Bash and turn them into Fish aliases, ugh...
-if test -e $oia_script_path
-    for line in (cat $oia_script_path)
-        if string match --regex "^alias *" $line >/dev/null
-            set line (string replace --regex "^alias *" "" $line)
-            set entry (string split "=" $line)
-
-            set value $entry[2]
-            set value (string replace -a "'" "" $value)
-            set value (string replace -a '"' "" $value)
-
-            alias $entry[1] "$value"
-        end
-    end
-end
-
 alias a applied
 alias ak "applied kubectl"
 alias ad "applied deploy"
