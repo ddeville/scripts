44eb527117a86489bc9244e11e0dbd8c1ab47350
Damien Deville <damien@ddeville.me>
2023-06-03 16:09:38 -0700

    Automatically run shfmt on buffer saves

--- config/common/.config/nvim/after/plugin/vim-shfmt.lua
+++ config/common/.config/nvim/after/plugin/vim-shfmt.lua
@@ -1,2 +1,11 @@
 vim.g.shfmt_fmt_on_save = 1
 vim.g.shfmt_extra_args = '--simplify --indent 2'
+
+-- `shfmt_fmt_on_save` doesn't seem to work so let's do it manually instead...
+local ag = vim.api.nvim_create_augroup('ShfmtAutoformat', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'sh',
+  group = ag,
+  nested = true,
+  command = [[autocmd BufWritePre <buffer> Shfmt]],
+})
