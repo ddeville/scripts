5f4b6a7d8d7a698a3ed2461661484b7d606fc41e
Damien Deville <damien@ddeville.me>
2023-06-02 18:16:23 -0700

    much faster check for whether pyenv virtualenv is installed
    The previous version was actually iterating through all installed
    virtualenvs...

--- config/common/.config/fish/conf.d/pyenv.fish
+++ config/common/.config/fish/conf.d/pyenv.fish
@@ -1,4 +1,4 @@
 set -gx PYENV_SHELL fish
 
 # NOTE(perf): This seems to take ~220ms to source on an M1 macbook air
-status --is-interactive; and pyenv virtualenvs >/dev/null 2>&1; and pyenv virtualenv-init - | source
+status --is-interactive; and pyenv virtualenv --help >/dev/null 2>&1; and pyenv virtualenv-init - | source
