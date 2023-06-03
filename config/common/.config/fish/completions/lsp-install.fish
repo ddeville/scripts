4fe22e1eb1ddf30013045cf9c1149310fb093a12
Damien Deville <damien@ddeville.me>
2023-06-03 15:25:07 -0700

    Install bash lsp

--- config/common/.config/fish/completions/lsp-install.fish
+++ config/common/.config/fish/completions/lsp-install.fish
@@ -1,2 +1,2 @@
-set -l commands all rust-analyzer clangd gopls tsserver pyright yamlls terraform-ls lua-ls
+set -l commands all rust-analyzer clangd gopls tsserver pyright yamlls terraform-ls lua-ls bash-ls
 complete -c lsp-install -f -a "$commands"
