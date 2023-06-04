002722774dc40ca7bc34725422f670757a514d80
Damien Deville <damien@ddeville.me>
2023-06-04 12:59:09 -0700

    Store vars in flat files since jq is slowish

--- config/common/.config/fish/functions/oai_buildbox_activate.fish
+++ config/common/.config/fish/functions/oai_buildbox_activate.fish
@@ -1,11 +1,10 @@
 function oai_buildbox_activate --description "Set the env variables necessary to use the remote buildbox"
-    set config_path "$HOME/.config/openai/buildbox/config.json"
+    set config_path "$HOME/.config/openai/buildbox"
 
     if test -e $config_path
-        # NOTE(perf): Using `jq` is slow and takes ~15ms for each `jq` command.
-        set -gx DOCKER_HOST (jq .DOCKER_HOST --raw-output $config_path)
+        set -gx DOCKER_HOST (cat "$config_path/DOCKER_HOST")
         set -gx DOCKER_TLS_VERIFY 1
-        set -gx DOCKER_CERT_PATH (jq .DOCKER_CERT_PATH --raw-output $config_path)
+        set -gx DOCKER_CERT_PATH (cat "$config_path/DOCKER_CERT_PATH")
     else
         if test "$argv[1]" != silent
             echo "ERROR: You need to add the Docker host path to $config_path"
--- config/common/.config/fish/functions/oai_set_api_key.fish
+++ config/common/.config/fish/functions/oai_set_api_key.fish
@@ -1,10 +1,9 @@
 function oai_set_api_key --description "Load and set OpenAI API key"
-    set config_path "$HOME/.config/openai/api_key.json"
+    set config_path "$HOME/.config/openai/api_key"
 
     if test -e $config_path
-        # NOTE(perf): Using `jq` is slow and takes ~15ms for each `jq` command
-        set -gx OPENAI_API_KEY (jq .OPENAI_API_KEY --raw-output $config_path)
-        set -gx OPENAI_API_BASE (jq .OPENAI_API_BASE --raw-output $config_path)
+        set -gx OPENAI_API_KEY (cat "$config_path/OPENAI_API_KEY")
+        set -gx OPENAI_API_BASE (cat "$config_path/OPENAI_API_BASE")
     else
         if test "$argv[1]" != silent
             echo "ERROR: You need to add your key to $config_path"
