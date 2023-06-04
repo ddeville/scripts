80b919c63673c711f52cfe16a97d7f0100d36e14
Damien Deville <damien@ddeville.me>
2023-06-04 14:05:30 -0700

    remove comments

--- config/common/.config/fish/functions/oai_tmux.fish
+++ config/common/.config/fish/functions/oai_tmux.fish
@@ -8,25 +8,21 @@ function oai_tmux --description "Create OAI tmux sessions"
         return 1
     end
 
-    # The API session
     command tmux new-session -D -s api -d -c ~/code/api
     command tmux rename-window -t api:1 repo
     command tmux new-window -t api -n editor -d -c ~/code/api
     command tmux send-keys -t api:2 "nvim ." Enter
 
-    # The openai-py session
     command tmux new-session -D -s openai-py -d -c ~/code/openai-python
     command tmux rename-window -t openai-py:1 repo
     command tmux new-window -t openai-py -n editor -d -c ~/code/openai-python
     command tmux send-keys -t openai-py:2 "nvim ." Enter
 
-    # The monorepo session
     command tmux new-session -D -s monorepo -d -c ~/code/openai
     command tmux rename-window -t monorepo:1 repo
     command tmux new-window -t monorepo -n editor -d -c ~/code/openai
     command tmux send-keys -t monorepo:2 "nvim ." Enter
 
-    # The scripts and notes session
     command tmux new-session -D -s scripts -d -c ~/scripts
     command tmux rename-window -t scripts:1 repo
     command tmux new-window -t scripts -n editor -d -c ~/scripts
@@ -34,7 +30,6 @@ function oai_tmux --description "Create OAI tmux sessions"
     command tmux new-window -t scripts -n notes -d -c ~/Desktop
     command tmux send-keys -t scripts:3 "nvim notes.txt" Enter
 
-    # Select the API session
     command tmux switch-client -t api
     command tmux select-window -t api:1
     command tmux attach -d -t api
