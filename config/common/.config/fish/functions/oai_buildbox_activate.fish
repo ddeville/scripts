913a33f953c641f451bcc014a85c20ae59568145
Damien Deville <damien@ddeville.me>
2023-06-04 13:09:31 -0700

    Source env vars from files directly
    Not sure why I didn't do it this way to start with :/

--- config/common/.config/fish/functions/oai_buildbox_activate.fish
+++ config/common/.config/fish/functions/oai_buildbox_activate.fish
@@ -1,10 +1,8 @@
 function oai_buildbox_activate --description "Set the env variables necessary to use the remote buildbox"
-    set config_path "$HOME/.config/openai/buildbox"
+    set config_path "$HOME/.config/openai/buildbox/docker.fish"
 
     if test -e $config_path
-        set -gx DOCKER_HOST (cat "$config_path/DOCKER_HOST")
-        set -gx DOCKER_TLS_VERIFY 1
-        set -gx DOCKER_CERT_PATH (cat "$config_path/DOCKER_CERT_PATH")
+        source "$config_path"
     else
         if test "$argv[1]" != silent
             echo "ERROR: You need to add the Docker host path to $config_path"
--- config/common/.config/fish/functions/oai_set_api_key.fish
+++ config/common/.config/fish/functions/oai_set_api_key.fish
@@ -1,9 +1,8 @@
 function oai_set_api_key --description "Load and set OpenAI API key"
-    set config_path "$HOME/.config/openai/api_key"
+    set config_path "$HOME/.config/openai/api_key.fish"
 
     if test -e $config_path
-        set -gx OPENAI_API_KEY (cat "$config_path/OPENAI_API_KEY")
-        set -gx OPENAI_API_BASE (cat "$config_path/OPENAI_API_BASE")
+        source "$config_path"
     else
         if test "$argv[1]" != silent
             echo "ERROR: You need to add your key to $config_path"
