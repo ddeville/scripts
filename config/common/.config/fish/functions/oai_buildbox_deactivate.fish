8f327a878278695ef200f8a3254e9a562d2c1ec8
Damien Deville <damien@ddeville.me>
2023-06-04 17:54:47 -0700

    Remove oai stuff

--- config/common/.config/fish/functions/oai_buildbox_deactivate.fish
+++
@@ -1,8 +0,0 @@
-function oai_buildbox_deactivate --description "Remove the remote buildbox env variables"
-    set --unexport DOCKER_HOST
-    set --erase DOCKER_HOST
-    set --unexport DOCKER_TLS_VERIFY
-    set --erase DOCKER_TLS_VERIFY
-    set --unexport DOCKER_CERT_PATH
-    set --erase DOCKER_CERT_PATH
-end
