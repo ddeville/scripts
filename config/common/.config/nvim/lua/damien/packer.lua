cfb4ed0ce2613ca89f27b76e4e7ef5252f69c312
Damien Deville <damien@ddeville.me>
2023-06-11 09:15:52 -0700

    No need for a requires

--- config/common/.config/nvim/lua/damien/packer.lua
+++ config/common/.config/nvim/lua/damien/packer.lua
@@ -66,14 +66,12 @@ packer.startup({
     -- Autocompletion
     use({
       'hrsh7th/nvim-cmp',
-      requires = {
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/vim-vsnip',
-        'hrsh7th/cmp-vsnip',
-      },
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/cmp-vsnip',
     })
 
     -- Treesitter
