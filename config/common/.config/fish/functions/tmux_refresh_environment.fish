8dbefeeba4d1f4bbe96c94c3bac54422105b386f
Damien Deville <damien@ddeville.me>
2023-06-11 22:39:37 -0700

    No need for quotes

--- config/common/.config/fish/functions/tmux_refresh_environment.fish
+++ config/common/.config/fish/functions/tmux_refresh_environment.fish
@@ -1,9 +1,9 @@
 function tmux_refresh_environment --description "Refresh the current environment variables in the shell from their tmux counterpart"
     for line in (tmux show-environment)
-        if string match -qr '^-.*' -- "$line"
-            set --global --erase (string replace "-" "" -- "$line")
+        if string match -qr '^-.*' -- $line
+            set --global --erase (string replace "-" "" -- $line)
         else
-            set --global --export (string replace -r '=.*' '' -- "$line") (string replace -r ".*=" "" -- "$line")
+            set --global --export (string replace -r '=.*' '' -- $line) (string replace -r ".*=" "" -- $line)
         end
     end
 end
