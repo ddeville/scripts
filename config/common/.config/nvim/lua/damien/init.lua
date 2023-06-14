07d52ef45ea8b0136000af5c7e136ea900a88aaf
Damien Deville <damien@ddeville.me>
2023-06-14 09:58:00 -0700

    Rename plugins to lazy
    Since we will have a folder for plugins

--- config/common/.config/nvim/lua/damien/init.lua
+++ config/common/.config/nvim/lua/damien/init.lua
@@ -2,6 +2,6 @@ require('damien.settings')
 require('damien.mappings')
 
 -- Install plugins after settings and mappings to make sure that plugins don't end up using wrong ones...
-require('damien.plugins')
+require('damien.lazy')
 
 require('damien.base16')
