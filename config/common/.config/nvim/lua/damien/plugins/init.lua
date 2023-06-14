357695b7ab6a90ca30f3c231ee6ff1d31e4b4270
Damien Deville <damien@ddeville.me>
2023-06-14 11:09:36 -0700

    Move nvim-cmp config to new file

--- config/common/.config/nvim/lua/damien/plugins/init.lua
+++ config/common/.config/nvim/lua/damien/plugins/init.lua
@@ -11,20 +11,6 @@ return {
     },
   },
 
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lua',
-      'hrsh7th/vim-vsnip',
-      'hrsh7th/cmp-vsnip',
-    },
-  },
-
   -- Treesitter
   {
     'nvim-treesitter/nvim-treesitter',
