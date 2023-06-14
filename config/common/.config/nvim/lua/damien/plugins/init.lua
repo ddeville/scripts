cde089a88b6bd7d67d56538744479519d1dbd709
Damien Deville <damien@ddeville.me>
2023-06-14 11:17:29 -0700

    Move telescope and treesitter to separate config

--- config/common/.config/nvim/lua/damien/plugins/init.lua
+++ config/common/.config/nvim/lua/damien/plugins/init.lua
@@ -10,32 +10,4 @@ return {
       { 'j-hui/fidget.nvim', tag = 'legacy' },
     },
   },
-
-  -- Treesitter
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = function()
-      pcall(require('nvim-treesitter.install').update({ with_sync = true }))
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-  },
-
-  -- Telescope
-  {
-    'nvim-telescope/telescope.nvim',
-    version = '*',
-    dependencies = {
-      'nvim-lua/popup.nvim',
-      'nvim-lua/plenary.nvim',
-    },
-  },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-    cond = function()
-      return vim.fn.executable('cmake') == 1
-    end,
-  },
 }
---
+++ config/common/.config/nvim/lua/damien/plugins/treesitter.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'bash',
+          'c',
+          'comment',
+          'cpp',
+          'css',
+          'dockerfile',
+          'fish',
+          'go',
+          'hcl',
+          'html',
+          'java',
+          'javascript',
+          'json',
+          'kotlin',
+          'lua',
+          'python',
+          'rust',
+          'toml',
+          'typescript',
+          'vim',
+          'vimdoc',
+          'yaml',
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+    build = function()
+      pcall(require('nvim-treesitter.install').update({ with_sync = true }))
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+  },
+}
