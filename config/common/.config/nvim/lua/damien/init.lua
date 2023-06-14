eab5614f9a3aeac8dd2fa426723c362c79467ebc
Damien Deville <damien@ddeville.me>
2023-06-13 20:53:11 -0700

    Load plugins last

--- config/common/.config/nvim/lua/damien/init.lua
+++ config/common/.config/nvim/lua/damien/init.lua
@@ -1,4 +1,7 @@
-require('damien.packer')
 require('damien.base16')
 require('damien.settings')
 require('damien.mappings')
+
+-- Install plugins last to make sure that things like mapping are set before they get a chance to
+-- get the incorrect ones...
+require('damien.packer')
