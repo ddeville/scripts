44fbdb65915aae462fa802e013f30b78bba68d89
Damien Deville <damien@ddeville.me>
2023-06-14 23:00:29 -0700

    Add a couple more env variables

--- config/common/.config/fish/config.fish
+++ config/common/.config/fish/config.fish
@@ -1,5 +1,8 @@
 set -gx CLICOLOR 1
 set -gx EDITOR nvim
+set -gx NAME "Damien Deville"
+set -gx EMAIL damien@ddeville.me
+set -gx TZ America/Los_Angeles
 
 # Sometimes this is not set correctly if only the Xcode CLI tools are installed
 if test (uname) = Darwin && ! test -n "$SDKROOT"
