0b2f0313ea283b3d506f102bac00b91f691434ee
Damien Deville <damien@ddeville.me>
2023-06-19 21:15:30 -0700

    Brew should have priority over xcode tools
    Since xcode tools are often old...

--- config/common/.config/fish/conf.d/path.fish
+++ config/common/.config/fish/conf.d/path.fish
@@ -29,14 +29,14 @@ add_to_path "$HOME/.local/bin"
 add_to_path "$CARGO_HOME/bin"
 add_to_path "$HOME/src/go/bin"
 
+# brew install its stuff there on M1 macs
+add_to_path /opt/homebrew/bin
+add_to_path /opt/homebrew/sbin
+
 # check whether xcode is installed and add its bin dir to the path if so
 if which xcode-select >/dev/null 2>&1; and set -l XC (xcode-select --print-path)
     add_to_path "$XC/usr/bin"
 end
-
-# brew install its stuff there on M1 macs
-add_to_path /opt/homebrew/bin
-add_to_path /opt/homebrew/sbin
 
 # we can now set the new entries in front of the path
 set -x PATH $PATH_ENTRIES $PATH
