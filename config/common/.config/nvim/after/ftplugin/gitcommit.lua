41e39b6ee39789d3668356ebf679345e82e575bd
Damien Deville <damien@ddeville.me>
2024-05-19 09:12:41 -0700

    Add comment for gitcommit

--- config/common/.config/nvim/after/ftplugin/gitcommit.lua
+++ config/common/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,3 @@
+-- Otherwise the commit message is red and overflow white when using gruvbox.
 vim.api.nvim_set_hl(0, 'gitcommitSummary', { link = 'gitcommitDiff' })
 vim.api.nvim_set_hl(0, 'gitcommitOverflow', { link = 'WarningMsg' })
