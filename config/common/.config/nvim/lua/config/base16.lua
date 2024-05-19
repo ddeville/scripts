d8d9fa037414de77b838bdf9103880eeb271e012
Damien Deville <damien@ddeville.me>
2024-05-18 22:24:26 -0700

    Switch to using nvim-based gruvbox project

--- config/common/.config/nvim/lua/config/base16.lua
+++ config/common/.config/nvim/lua/config/base16.lua
@@ -1,8 +1,13 @@
-local state_dir = os.getenv('XDG_STATE_HOME') or os.getenv('HOME') .. '/.local/state'
-
--- setup base16 colorscheme
-local set_theme_path = state_dir .. '/base16/set_theme.lua'
-if vim.fn.filereadable(vim.fn.expand(set_theme_path)) == 1 then
-  vim.g.base16colorspace = 256
-  vim.cmd('source ' .. set_theme_path)
-end
+-- NOTE: We don't use base16 in nvim anymore and instead use the nvim gruvbox plugin
+--       which is much better and also happens to follow all nvim updates, in particular
+--       around tree-sitter. This also means that updating the base16 theme in the shell
+--       will no longer update the nvim theme but I also basically never used that...
+--
+-- local state_dir = os.getenv('XDG_STATE_HOME') or os.getenv('HOME') .. '/.local/state'
+--
+-- -- setup base16 colorscheme
+-- local set_theme_path = state_dir .. '/base16/set_theme.lua'
+-- if vim.fn.filereadable(vim.fn.expand(set_theme_path)) == 1 then
+--   vim.g.base16colorspace = 256
+--   vim.cmd('source ' .. set_theme_path)
+-- end
--- config/common/.config/nvim/lua/config/plugins/appearance.lua
+++ config/common/.config/nvim/lua/config/plugins/appearance.lua
@@ -1,23 +1,65 @@
 return {
   'mhinz/vim-startify',
-  'tinted-theming/base16-vim',
   'ddeville/vim-base16-lightline',
   {
-    'itchyny/lightline.vim',
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup({
+        terminal_colors = true,
+        undercurl = false,
+        underline = false,
+        bold = false,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = false,
+        contrast = '', -- can be "hard", "soft" or empty string
+      })
+      vim.cmd('colorscheme gruvbox')
+    end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
     config = function()
       -- don't show `-- INSERT --` since we have a fancy status line
       vim.opt.showmode = false
 
-      -- color scheme and items
-      vim.g.lightline = {
-        colorscheme = 'base16',
-        active = {
-          left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'filename', 'modified' }, { 'lsp_status' } },
+      -- By default the gruvbox theme has_overrides uses cyan for insert and green for
+      -- command but I'm so used to it being the opposite...
+      local gruvbox = require('lualine.themes.gruvbox')
+      gruvbox.command.a.bg = '#83a598'
+      gruvbox.insert.a.bg = '#b8bb26'
+
+      require('lualine').setup({
+        options = {
+          icons_enabled = false,
+          theme = gruvbox,
+          component_separators = { left = '|', right = '|' },
+          section_separators = { left = '', right = '' },
         },
-        component_function = {
-          gitbranch = 'fugitive#Head',
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'diagnostics', 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
         },
-      }
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { 'filename' },
+          lualine_x = { 'location' },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        extensions = { 'quickfix' },
+      })
     end,
   },
 }
