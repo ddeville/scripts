595128699f70bd757a1c9b5d98910ed96247f76b
Damien Deville <damien@ddeville.me>
2024-08-04 14:50:02 -0700

    Install latest node

--- install/devbox/install.sh
+++ install/devbox/install.sh
@@ -16,7 +16,12 @@ export XDG_STATE_HOME="$HOME/.local/state"
 mkdir -p "$XDG_DATA_HOME"
 mkdir -p "$XDG_STATE_HOME"
 
+# Latest git
 sudo add-apt-repository ppa:git-core/ppa
+
+# Current LTS nodejs
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+
 sudo apt-get update
 
 sudo apt-get -y install black \
