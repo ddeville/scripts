b13dcd12ef426e153ecda6647466abd723019f92
Damien Deville <damien@ddeville.me>
2024-08-12 12:39:05 -0700

    Fix golang binary install path

--- install/devbox/install.sh
+++ install/devbox/install.sh
@@ -181,7 +181,7 @@ curl -L https://github.com/mvdan/sh/releases/download/"${SHFMT_VERSION}"/shfmt_"
 chmod +x shfmt && sudo mv shfmt "$PREFIX"/bin/shfmt
 
 # gopls
-/usr/local/go/bin/go install golang.org/x/tools/gopls@latest
+"$XDG_TOOLCHAINS_HOME/go/current/bin/go" install golang.org/x/tools/gopls@latest
 
 #########################
 ######### Shell #########
