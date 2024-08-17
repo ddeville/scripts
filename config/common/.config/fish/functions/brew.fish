7019df404b4b699a2da4b9d0d07e6fd15b788163
Damien Deville <damien@ddeville.me>
2024-08-17 12:36:32 -0700

    New brew wrapping function to keep filtered symlinks up to date

---
+++ config/common/.config/fish/functions/brew.fish
@@ -0,0 +1,18 @@
+function pyenv
+    if not type -fq brew
+        echo "brew is not installed, this is just a fish function wrapper!"
+        return 1
+    end
+
+    set cmd $argv[1]
+
+    command brew $argv
+
+    switch "$cmd"
+        case install uninstall upgrade
+            if test (uname) = Linux; and test -e /home/linuxbrew/.filtered; and type -fq filter-brew-leaf-packages
+                echo "Updating symlinks for filtered packages..."
+                command filter-brew-leaf-packages
+            end
+    end
+end
