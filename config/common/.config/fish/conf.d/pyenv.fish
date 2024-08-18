be155a4d8a3fac497c80085cca6106bc6bb7078b
Damien Deville <damien@ddeville.me>
2024-08-18 00:41:19 -0700

    Don't try to use if pyenv isn't installed

--- config/common/.config/fish/conf.d/pyenv.fish
+++ config/common/.config/fish/conf.d/pyenv.fish
@@ -1,3 +1,3 @@
 set -gx PYENV_SHELL fish
 
-status --is-interactive; and source (pyenv virtualenv-init - 2>/dev/null|psub)
+status --is-interactive; and type -fq pyenv; and source (pyenv virtualenv-init - 2>/dev/null|psub)
