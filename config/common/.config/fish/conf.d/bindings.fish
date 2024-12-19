850385263ebd421321f0d8dd9eb4fcf5e7b92cf8
Damien Deville <damien@ddeville.me>
2024-12-19 11:15:55 -0800

    Bring back block cursor on insert

--- config/common/.config/fish/conf.d/bindings.fish
+++ config/common/.config/fish/conf.d/bindings.fish
@@ -1,5 +1,8 @@
 # use the vim key bindings
 set -gx fish_key_bindings fish_vi_key_bindings
 
+# in v4 fish correctly sets the vi insert cursor to a line but I'm too used to it being a block
+set fish_cursor_insert block
+
 # ctrl-y to accept autocomplete suggestion
 bind -M insert \cy accept-autosuggestion
