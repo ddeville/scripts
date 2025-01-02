7c83793858731b6b3aca97d28366bc5a42af4834
Damien Deville <damien@ddeville.me>
2025-01-01 18:36:06 -0800

    Reformat

--- config/common/.config/wezterm/wezterm.lua
+++ config/common/.config/wezterm/wezterm.lua
@@ -1,31 +1,31 @@
 local wezterm = require('wezterm')
 
-local config = {
-  color_scheme = 'Gruvbox dark, soft (base16)',
-  bold_brightens_ansi_colors = true,
-  font = wezterm.font('AnonymicePro Nerd Font'),
-  font_rules = {
-    {
-      intensity = 'Bold',
-      italic = false,
-      font = wezterm.font('AnonymicePro Nerd Font', { weight = 'Regular' }),
-    },
+local cfg = {}
 
-    {
-      intensity = 'Bold',
-      italic = true,
-      font = wezterm.font('AnonymicePro Nerd Font', { weight = 'Regular' }),
-    },
+-- Colorscheme
+cfg.color_scheme = 'Gruvbox dark, soft (base16)'
+cfg.bold_brightens_ansi_colors = true
+
+-- Font
+cfg.font = wezterm.font('AnonymicePro Nerd Font')
+cfg.font_rules = {
+  {
+    intensity = 'Bold',
+    italic = false,
+    font = wezterm.font('AnonymicePro Nerd Font', { weight = 'Regular' }),
   },
-  font_size = 14,
-  line_height = 1.15,
-  hide_tab_bar_if_only_one_tab = true,
-  window_padding = {
-    left = 4,
-    right = 4,
-    top = 2,
-    bottom = 2,
+
+  {
+    intensity = 'Bold',
+    italic = true,
+    font = wezterm.font('AnonymicePro Nerd Font', { weight = 'Regular' }),
   },
 }
+cfg.font_size = 14
+cfg.line_height = 1.15
 
-return config
+-- Window
+cfg.hide_tab_bar_if_only_one_tab = true
+cfg.window_padding = { left = 4, right = 4, top = 2, bottom = 2 }
+
+return cfg
