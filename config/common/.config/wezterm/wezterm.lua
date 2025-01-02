2cd1af7ff479b33db38ebc2c4d1ca09b8e7e4158
Damien Deville <damien@ddeville.me>
2025-01-01 21:15:38 -0800

    Tweak a few settings and different font size between macos and linux

--- config/common/.config/wezterm/wezterm.lua
+++ config/common/.config/wezterm/wezterm.lua
@@ -1,6 +1,13 @@
 local wezterm = require('wezterm')
 
+local is_macos = string.find(wezterm.target_triple, 'darwin')
+
 local cfg = {}
+
+cfg.term = 'wezterm'
+cfg.audible_bell = 'Disabled'
+cfg.check_for_updates = false
+cfg.window_close_confirmation = 'NeverPrompt'
 
 -- Colorscheme
 cfg.color_scheme = 'Gruvbox dark, soft (base16)'
@@ -21,11 +28,13 @@ cfg.font_rules = {
     font = wezterm.font('AnonymicePro Nerd Font', { weight = 'Regular' }),
   },
 }
-cfg.font_size = 14
+cfg.font_size = is_macos and 14 or 10
 cfg.line_height = 1.15
 
 -- Window
 cfg.hide_tab_bar_if_only_one_tab = true
 cfg.window_padding = { left = 4, right = 4, top = 2, bottom = 2 }
+cfg.max_fps = 120
+cfg.prefer_egl = true
 
 return cfg
