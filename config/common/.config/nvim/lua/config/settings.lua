8eaf8411b608c27b419e2dd91b09ed6d1a97cd11
Damien Deville <damien@ddeville.me>
2025-08-09 16:16:51 -0700

    Fix warning

--- config/common/.config/nvim/lua/config/settings.lua
+++ config/common/.config/nvim/lua/config/settings.lua
@@ -111,7 +111,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('LspDisableSemanticTokensProvider', {}),
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    client.server_capabilities.semanticTokensProvider = nil
+    if client and client.server_capabilities then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
   end,
 })
 
