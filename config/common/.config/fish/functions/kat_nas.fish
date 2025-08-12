fdf025f70dea78070a97e9e61df8a7a90f408bd9
Damien Deville <damien@ddeville.me>
2025-08-12 15:49:15 -0700

    Remove unused function

--- config/common/.config/fish/functions/kat_nas.fish
+++
@@ -1,3 +0,0 @@
-function kat_nas --description "Connect to Synology and start/join a tmux session"
-    command ssh damien@nas.lab.kattungar.net -t "/usr/local/bin/tmux new-session -D -s nas -c ~ || /usr/local/bin/tmux attach -d -t nas"
-end
