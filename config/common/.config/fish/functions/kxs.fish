045264377b1eada6bc3946a050199a5ec6cf2ace
Damien Deville <damien@ddeville.me>
2025-08-14 16:10:51 -0700

    New function to change context and ns at once

---
+++ config/common/.config/fish/functions/kxs.fish
@@ -0,0 +1,8 @@
+function kxs
+    if test (count $argv) -ne 2
+        echo "Usage: kx <context> <ns>"
+        return 1
+    end
+    kubectl config use-context $argv[1]
+    kubectl config set-context --current --namespace=$argv[2]
+end
