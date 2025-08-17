2ca66fd59e782718ba0171567493845eef9f836c
Damien Deville <damien@ddeville.me>
2025-08-16 18:07:01 -0700

    Update devbox packages

--- install/devbox/install.sh
+++ install/devbox/install.sh
@@ -14,11 +14,11 @@ fi
 ############## Setup ##############
 ###################################
 
-PYTHON_VERSION=3.11.8
-RUST_VERSION=1.76.0
-GOLANG_VERSION=1.22.2
-NODE_VERSION=22.6.0
-TERRAFORM_VERSION=1.6.4
+PYTHON_VERSION=3.12.9
+RUST_VERSION=1.89.0
+GOLANG_VERSION=1.25.0
+NODE_VERSION=24.6.0
+TERRAFORM_VERSION=1.12.2
 
 export XDG_CONFIG_HOME="$HOME/.config" && mkdir -p "$XDG_CONFIG_HOME"
 export XDG_DATA_HOME="$HOME/.local/share" && mkdir -p "$XDG_DATA_HOME"
