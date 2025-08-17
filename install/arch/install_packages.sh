d75d3c7210a138139a2f08c0afb4dff116c02a06
Damien Deville <damien@ddeville.me>
2025-08-16 17:50:50 -0700

    Move pkglist to config for arch

--- install/arch/install_packages.sh
+++ install/arch/install_packages.sh
@@ -3,14 +3,15 @@
 set -eu
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+pkglist_dir=$(realpath "$script_dir/../../config/linux/.config/pkglist")
 
 # Install all the base packages
-readarray -t base_packages < <(grep -Ev "^#|^$" "$script_dir/base_packages.txt")
+readarray -t base_packages < <(grep -Ev "^#|^$" "$pkglist_dir/base_packages.txt")
 sudo pacman -Syy
 sudo pacman -S --needed "${base_packages[@]}"
 
 # Install AUR packages with Paru
-readarray -t aur_packages < <(grep -Ev "^#|^$" "$script_dir/aur_packages.txt")
+readarray -t aur_packages < <(grep -Ev "^#|^$" "$pkglist_dir/aur_packages.txt")
 /usr/bin/paru -Syy
 /usr/bin/paru -S --needed "${aur_packages[@]}"
 
