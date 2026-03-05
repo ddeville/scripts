b09f8bad18c5739e9e5431983d2aa5e0a69a087f
Damien Deville <damien@ddeville.me>
2026-03-04 21:05:15 -0800

    Include name field
    Otherwise it's treated as a special field that doesn't get included.

--- config/common/.config/fish/conf.d/abbr.fish
+++ config/common/.config/fish/conf.d/abbr.fish
@@ -12,4 +12,4 @@ alias e nvim
 alias tf terraform
 
 # NOTE: Not an alias because it gets turned into a function that messes up stdin/stdout by introducing some buffering.
-abbr jll "jl --color --max-field-length=0"
+abbr jll "jl --color --max-field-length=0 --include-fields=name"
