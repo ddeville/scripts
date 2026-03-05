59351cecb54377ce8aee379016be5b3706025490
Damien Deville <damien@ddeville.me>
2026-03-04 20:55:44 -0800

    Switch jll to an abbr

--- config/common/.config/fish/conf.d/abbr.fish
+++ config/common/.config/fish/conf.d/abbr.fish
@@ -8,7 +8,8 @@ alias oo "open ."
 alias topc "top -o cpu"
 alias topm "top -o mem"
 
-alias jll "jl --color --max-field-length=0"
-
 alias e nvim
 alias tf terraform
+
+# NOTE: Not an alias because it gets turned into a function that messes up stdin/stdout by introducing some buffering.
+abbr jll "jl --color --max-field-length=0"
