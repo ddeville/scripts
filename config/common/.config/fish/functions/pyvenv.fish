f1d4c9b26a881d4c9fd2b5c1fc66fd790a96d20e
Damien Deville <damien@ddeville.me>
2026-03-16 16:22:15 -0700

    Add a way to activate global python venv

---
+++ config/common/.config/fish/functions/pyvenv.fish
@@ -0,0 +1,17 @@
+function pyvenv --description "Activate the global Python venv"
+    set -l toolchains_home "$XDG_TOOLCHAINS_HOME"
+    if test -z "$toolchains_home"
+        set toolchains_home "$HOME/.local/toolchains"
+    end
+
+    set -l global_venv "$toolchains_home/python/global/venv"
+    set -l activate_path "$global_venv/bin/activate.fish"
+
+    if not test -e "$activate_path"
+        echo "Global Python venv not found at $global_venv. Run: pypkg"
+        return 1
+    end
+
+    set -e PYTHONHOME
+    source "$activate_path"
+end
