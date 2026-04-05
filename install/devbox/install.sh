e1fadf828ca78faec119093b496a58844d2748db
Damien Deville <damien@ddeville.me>
2026-04-05 12:20:03 -0700

    No password for current user

--- install/devbox/install.sh
+++ install/devbox/install.sh
@@ -12,7 +12,10 @@ fi
 
 echo "Running devbox install preflight"
 sudo -v
-sudo adduser $USER sudo
+
+sudo adduser "$USER" sudo
+echo "${USER} ALL=(ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/90-nopasswd-${USER}" >/dev/null
+sudo chmod 0440 "/etc/sudoers.d/90-nopasswd-${USER}"
 
 export XDG_CONFIG_HOME="$HOME/.config" && mkdir -p "$XDG_CONFIG_HOME"
 export XDG_DATA_HOME="$HOME/.local/share" && mkdir -p "$XDG_DATA_HOME"
