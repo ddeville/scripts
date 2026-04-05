ae54ff08c9039f8f23e8ecd304c388c16e935640
Damien Deville <damien@ddeville.me>
2026-04-05 12:17:51 -0700

    Add current user to sudoers

--- install/devbox/install.sh
+++ install/devbox/install.sh
@@ -10,6 +10,10 @@ if [ "$(id -u)" -eq 0 ]; then
   exit 1
 fi
 
+echo "Running devbox install preflight"
+sudo -v
+sudo adduser $USER sudo
+
 export XDG_CONFIG_HOME="$HOME/.config" && mkdir -p "$XDG_CONFIG_HOME"
 export XDG_DATA_HOME="$HOME/.local/share" && mkdir -p "$XDG_DATA_HOME"
 export XDG_STATE_HOME="$HOME/.local/state" && mkdir -p "$XDG_STATE_HOME"
