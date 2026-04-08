9abd702b4bcc0b66ddfcd6174536d90d091f0a61
Damien Deville <damien@ddeville.me>
2026-04-07 23:15:19 -0700

    Use vim.filetype.add instead

--- config/common/.config/nvim/lua/config/ft.lua
+++ config/common/.config/nvim/lua/config/ft.lua
@@ -1,16 +1,9 @@
--- setup filetype for tiltfile
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = { 'Tiltfile', '*.Tiltfile' },
-  callback = function()
-    vim.bo.filetype = 'tiltfile'
-  end,
-})
-
--- setup filetype for Brewfile
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = { 'Brewfile' },
-  callback = function()
-    vim.bo.filetype = 'brewfile'
-    vim.bo.syntax = 'brewfile'
-  end,
+vim.filetype.add({
+  filename = {
+    Brewfile = 'brewfile',
+    Tiltfile = 'tiltfile',
+  },
+  pattern = {
+    ['.*%.Tiltfile'] = 'tiltfile',
+  },
 })
