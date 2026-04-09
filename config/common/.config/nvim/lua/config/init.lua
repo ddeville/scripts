e8608ba1d83c9fb97146fd7b5105e24b7c0a3a05
Damien Deville <damien@ddeville.me>
2026-04-08 23:27:46 -0700

    Remove legacy base16 logic in neovim

--- config/common/.config/nvim/lua/config/init.lua
+++ config/common/.config/nvim/lua/config/init.lua
@@ -4,7 +4,4 @@ require('config.mappings')
 -- Install plugins after mappings to make sure that plugins don't end up using old wrong ones...
 require('config.lazy')
 
--- base16 has to come before settings since they might change highlights that would be wrong without it...
-require('config.base16')
-
 require('config.settings')
