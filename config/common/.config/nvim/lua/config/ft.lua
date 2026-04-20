4460213646c0bc4255b96a335691f03db84e2ed3
Damien Deville <damien@ddeville.me>
2026-04-19 17:57:56 -0700

    More filetypes

--- config/common/.config/nvim/lua/config/ft.lua
+++ config/common/.config/nvim/lua/config/ft.lua
@@ -2,8 +2,11 @@ vim.filetype.add({
   filename = {
     Brewfile = 'brewfile',
     Tiltfile = 'tiltfile',
+    ['go.work'] = 'gowork',
   },
   pattern = {
     ['.*%.Tiltfile'] = 'tiltfile',
+    ['.*%.gotmpl'] = 'gotmpl',
+    ['.*%.go%.tmpl'] = 'gotmpl',
   },
 })
