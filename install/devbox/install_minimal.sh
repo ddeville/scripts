375a0bc23f5fb777ca9a76f81a0c87ed1de46dbc
Damien Deville <damien@ddeville.me>
2026-05-05 21:30:23 -0700

    Remove toolchain env var from minimal

--- install/devbox/install_minimal.sh
+++ install/devbox/install_minimal.sh
@@ -12,7 +12,6 @@ export PATH="$HOME/.local/bin:$PATH"
 export XDG_CONFIG_HOME="$HOME/.config" && mkdir -p "$XDG_CONFIG_HOME"
 export XDG_DATA_HOME="$HOME/.local/share" && mkdir -p "$XDG_DATA_HOME"
 export XDG_STATE_HOME="$HOME/.local/state" && mkdir -p "$XDG_STATE_HOME"
-export XDG_TOOLCHAINS_HOME="$HOME/.local/toolchains" && mkdir -p "$XDG_TOOLCHAINS_HOME"
 
 INSTALL_TMPDIR="$(mktemp -d)"
 cd "$INSTALL_TMPDIR" || exit
